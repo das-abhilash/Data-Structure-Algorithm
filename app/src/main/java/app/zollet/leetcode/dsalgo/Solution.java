5186e15e1f8d85f7f428bc504730120853faa79a
abhilashdas <abhilash.das@paytmmoney.com>
2020-11-10 16:19:18 +0530

    solve "Flipping an Image"

--- app/src/main/java/app/zollet/leetcode/dsalgo/Solution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/Solution.java
@@ -1,45 +1,28 @@
 package app.zollet.leetcode.dsalgo;
 
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-import app.zollet.leetcode.dsalgo.util.TreeNodeUtill;
-
 public class Solution {
 
-  public void execute() {
+    public void execute() {
 
-    TreeNode root = TreeNodeUtill.insertLevelOrder(new int[]{8, 3, 10, 1, 6, -1, 14, -1, -1, 4, 7, 13}, new TreeNode(8), 0);
-    int s = maxAncestorDiff(root);
-  }
-
-  int a = 0;
-
-  public int maxAncestorDiff(TreeNode root) {
-    a = 0;
-    solve(root);
-    return a;
-  }
-
-  private int[] solve(TreeNode root) {
-    if (root == null) return new int[]{0, 0};
-
-    int[] left = solve(root.left);
-    int[] right = solve(root.right);
-
-    if (root.left == null && root.right != null) {
-      a = Math.max(a, Math.max(Math.abs(root.val - right[0]), Math.abs(root.val - right[1])));
-      return new int[]{Math.max(root.val, right[0]), Math.min(root.val, right[1])};
-    } else if (root.left != null && root.right == null) {
-      a = Math.max(a, Math.max(Math.abs(root.val - left[0]), Math.abs(root.val - left[1])));
-      return new int[]{Math.max(root.val, left[0]), Math.min(root.val, left[1])};
-    } else if (root.left == null) {
-      return new int[]{root.val, root.val};
-    } else {
-
-      int one = Math.abs(root.val - Math.max(left[0], right[0]));
-      int two = Math.abs(root.val - Math.min(left[1], right[1]));
-
-      a = Math.max(a, Math.max(one, two));
-      return new int[]{Math.max(root.val, Math.max(left[0], right[0])), Math.min(root.val, Math.min(right[1], left[1]))};
     }
-  }
+
+    public int[][] flipAndInvertImage(int[][] A) {
+
+        for (int i = 0; i < A.length; i++) {
+            int l = 0;
+            int r = A[i].length - 1;
+            int[] a = A[i];
+            while (l <= r) {
+                int L = a[l];
+                int R = a[r];
+
+                a[l] = R ^ 1;
+                a[r] = L ^ 1;
+                l++;
+                r--;
+            }
+        }
+        return A;
+    }
+
 }
