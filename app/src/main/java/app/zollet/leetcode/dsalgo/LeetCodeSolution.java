fa5d71a2ca532e641e5529caee189d1c92bdad44
abhilashdas <abhilash.das@paytmmoney.com>
2020-11-16 16:29:59 +0530

    solve "Longest Mountain in Array"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,29 +1,39 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-
 public class LeetCodeSolution {
 
     public void execute() {
+        int a = longestMountain(new int[]{3, 4, 3, 5, 4, 6, 3, 3, 2, 4, 2});
     }
 
-    int sum = 0;
+    public int longestMountain(int[] A) {
 
-    public int rangeSumBST(TreeNode root, int low, int high) {
-        sum = 0;
-        solve(root, low, high);
-        return sum;
-    }
+        int answer = 0;
+        int in = 0;
+        int de = 0;
 
-    private void solve(TreeNode root, int low, int high) {
-        if (root == null) return;
-
-        if (root.val >= low && root.val <= high) {
-            sum += root.val;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) {
+                if (de > 0) {
+                    de = 0;
+                    in = 0;
+                }
+                if (in == 0) in = 2;
+                else in++;
+            } else if (A[i] < A[i - 1]) {
+                if (de == 0)
+                    de = 2;
+                else
+                    de++;
+                if (in != 0)
+                    answer = Math.max(answer, in + de - 1);
+            } else {
+                in = 0;
+                de = 0;
+            }
         }
-        solve(root.left, low, high);
-        solve(root.right, low, high);
+        return answer;
     }
 
 }
