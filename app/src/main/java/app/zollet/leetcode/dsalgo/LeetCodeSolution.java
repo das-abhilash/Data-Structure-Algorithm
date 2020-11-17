464e4b2083eebc050d9b7108faecad0f8bae8fd4
abhilashdas <abhilash.das@paytmmoney.com>
2020-11-17 13:45:29 +0530

    solve "Time Based Key-Value Store"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,9 +1,42 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class LeetCodeSolution {
 
     public void execute() {
+    }
+
+    class TimeMap {
+
+        Map<String, TreeMap<Integer, String>> map;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public TimeMap() {
+            map = new HashMap<>();
+        }
+
+        public void set(String key, String value, int timestamp) {
+
+            TreeMap<Integer, String> treeMap = map.getOrDefault(key, new TreeMap<>());
+            treeMap.put(timestamp, value);
+            map.put(key, treeMap);
+        }
+
+        public String get(String key, int timestamp) {
+
+            if (!map.containsKey(key)) return "";
+
+            TreeMap<Integer, String> treeMap = map.get(key);
+
+            Map.Entry<Integer, String> e = treeMap.floorEntry(timestamp);
+            return e == null ? "" : e.getValue();
+        }
     }
 
 }
