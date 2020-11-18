6f9ce923004b161a08b27842adc74206b83df213
abhilashdas <abhilashmyworld@gmail.com>
2020-11-18 21:50:24 +0530

    solve "Sum of Even Numbers After Queries"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,14 +1,36 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
 public class LeetCodeSolution {
 
     public void execute() {
+
+    }
+
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+
+        int[] answer = new int[A.length];
+        int sum = 0;
+        for (int value : A) {
+            if (value % 2 == 0) sum += value;
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int index = queries[i][1];
+            int val = queries[i][0];
+
+            int temp = A[index];
+
+            if (temp % 2 == 0) sum -= temp;
+
+            A[index] = val + temp;
+
+            if (A[index] % 2 == 0) {
+                sum -= A[index];
+            }
+            answer[i] = sum;
+        }
+        return answer;
     }
 
 }
