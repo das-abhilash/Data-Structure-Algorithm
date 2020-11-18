7d5c8a81c3dfd1b3de5bf11f5617a9179d85c7b0
abhilashdas <abhilashmyworld@gmail.com>
2020-11-18 22:22:32 +0530

    solve "Smallest String Starting From Leaf"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,36 +1,34 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+import app.zollet.leetcode.dsalgo.util.TreeNodeUtill;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
+        TreeNode root = TreeNodeUtill.insertLevelOrder(new int[]{2, 2, 1, -1, 1, 0, -1, 0}, new TreeNode(2), 0);
+        String a = smallestFromLeaf(root);
+
     }
 
-    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+    public String smallestFromLeaf(TreeNode root) {
+        if (root == null) return "";
 
-        int[] answer = new int[A.length];
-        int sum = 0;
-        for (int value : A) {
-            if (value % 2 == 0) sum += value;
+        String left = smallestFromLeaf(root.left);
+        String right = smallestFromLeaf(root.right);
+
+        char c = ((char) (root.val + 'a'));
+        String lex;
+        if (left.equals("")) {
+            lex = right + c;
+        } else if (right.equals("")) {
+            lex = left + c;
+        } else {
+            lex = (left + c).compareTo(right + c) > 0 ? right + c : left + c;
         }
-
-        for (int i = 0; i < queries.length; i++) {
-            int index = queries[i][1];
-            int val = queries[i][0];
-
-            int temp = A[index];
-
-            if (temp % 2 == 0) sum -= temp;
-
-            A[index] = val + temp;
-
-            if (A[index] % 2 == 0) {
-                sum -= A[index];
-            }
-            answer[i] = sum;
-        }
-        return answer;
+        return lex;
     }
 
 }
