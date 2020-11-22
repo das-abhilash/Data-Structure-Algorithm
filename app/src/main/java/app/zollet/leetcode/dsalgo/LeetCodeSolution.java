c4f81757c30e6ba8df8faf68bb89d120228ede76
abhilashdas <abhilashmyworld@gmail.com>
2020-11-22 08:07:39 +0530

    solve "Check If Two String Arrays are Equivalent"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -3,4 +3,20 @@ package app.zollet.leetcode.dsalgo;
 public class LeetCodeSolution {
     public void execute() {
     }
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : word1) {
+            sb.append(s);
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        for (String s : word2) {
+            sb1.append(s);
+        }
+
+        return sb.toString().equals(sb1.toString());
+    }
+
 }
