dfd205b304eba1b5a8d189a6a16b18887feae56d
abhilashdas <abhilashmyworld@gmail.com>
2020-11-24 19:38:16 +0530

    solve "Basic Calculator II"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,56 +1,77 @@
 package app.zollet.leetcode.dsalgo;
 
-import android.util.Log;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-import app.zollet.leetcode.dsalgo.util.TreeNodeUtill;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
 public class LeetCodeSolution {
     public void execute() {
-        TreeNode root = TreeNodeUtill.insertLevelOrder(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, new TreeNode(1), 0);
-        rob(root);
+        int a = calculate("4/3+2");
     }
 
-    public int rob(TreeNode root) {
-        if (root == null) return 0;
-        Map<String, Integer> map = new HashMap<>();
+    public int calculate(String s) {
 
+        String in = s.replaceAll(" ", "");
+        Stack<Character> sign = new Stack<>();
+        Stack<Integer> val = new Stack<>();
 
-        solve(root, 0, 0, map);
+        Set<Character> cc = new HashSet<Character>();
+        cc.add('+');
+        cc.add('-');
+        cc.add('*');
+        cc.add('/');
 
+        for (int i = 0; i < in.length(); i++) {
+            if (cc.contains(in.charAt(i))) {
+                if (in.charAt(i) == '*') {
+                    int v = 0;
+                    i++;
+                    while (i < in.length() && !cc.contains(in.charAt(i))) {
+                        v = (v * 10 + Integer.parseInt(String.valueOf(in.charAt(i))));
+                        i++;
+                    }
+                    int jj = val.pop() * v;
+                    val.push(jj);
+                    if (i != in.length())
+                        i--;
+                } else if (in.charAt(i) == '/') {
+                    int v = 0;
+                    i++;
+                    while (i < in.length() && !cc.contains(in.charAt(i))) {
+                        v = (v * 10 + Integer.parseInt(String.valueOf(in.charAt(i))));
+                        i++;
+                    }
 
-        return map.get(0 + "-" + 0 + "-" + root.val);
+                    int jj = val.pop() / v;
+                    val.push(jj);
+                    if (i != in.length())
+                        i--;
+                } else {
+                    sign.push(in.charAt(i));
+                }
+            } else {
 
-    }
-
-    private int solve(TreeNode node, int x, int y, Map<String, Integer> map) {
-        if (node == null) {
-            map.put(x + "-" + y + "-" + 0, 0);
-            return 0;
+                int v = 0;
+                while (i < in.length() && !cc.contains(in.charAt(i))) {
+                    v = (v * 10 + Integer.parseInt(String.valueOf(in.charAt(i))));
+                    i++;
+                }
+                val.push(v);
+                if (i != in.length())
+                    i--;
+            }
         }
 
-        if (map.containsKey(x + "-" + y + "-" + node.val))
-            return map.get(x + "-" + y + "-" + node.val);
-
-        Log.d("mehbla", node.val + "");
-
-        int cl = solve(node.left, x - 1, y + 1, map);
-        int cr = solve(node.right, x + 1, y + 1, map);
-
-        int c = cl + cr;
-
-        int ccll = (node.left == null) ? 0 : solve(node.left.left, x - 2, y + 2, map);
-        int cclr = (node.left == null) ? 0 : solve(node.left.right, x, y + 2, map);
-        int ccrl = (node.right == null) ? 0 : solve(node.right.left, x, y + 2, map);
-        int ccrr = (node.right == null) ? 0 : solve(node.right.right, x + 2, y + 2, map);
-
-
-        int a = Math.max(cl + cr, ccll + cclr + ccrl + ccrr + node.val);
-        map.put(x + "-" + y + "-" + node.val, a);
-        return a;
+        int aa = 0;
+        int mm = 0;
+        while (val.size() > 1) {
+            int v = val.pop();
+            int si = sign.pop();
+            if (si == '+')
+                aa += v;
+            else
+                mm += v;
+        }
+        return val.pop() + aa - mm;
     }
-
 }
