b6bb7ef7003d4fe61f238da6062d79babcf16f83
abhilashdas <abhilashmyworld@gmail.com>
2020-11-26 17:58:26 +0530

    solve "Longest Substring with At Least K Repeating Characters"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,62 +1,64 @@
 package app.zollet.leetcode.dsalgo;
 
+import java.util.Arrays;
+
 public class LeetCodeSolution {
 
     public void execute() {
+        int a = longestSubstring("aaabb", 3);
     }
 
-    public int numRookCaptures(char[][] board) {
+    public int longestSubstring(String s, int k) {
+        if (k > s.length()) return 0;
+        if (s.length() == 0) return 0;
+        if (k == 0 || k == 1) return s.length();
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'R') {
-                    return find(board, i, j);
-                }
-            }
+        int[] c = new int[26];
+        Arrays.fill(c, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i) - 'a']++;
+
         }
-        return 0;
-    }
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 26; i++) {
+            max = Math.max(max, c[i]);
+            if (c[i] != 0)
+                min = Math.min(min, c[i]);
+        }
 
-    private int find(char[][] board, int i, int j) {
+        if (max < k) return 0;
+        if (min >= k) return s.length();
+        if (k == s.length() && max == k) return s.length();
+
         int a = 0;
 
-        for (int k = i - 1; k >= 0; k--) {
-            if (board[k][j] == 'p') {
-                a++;
-                break;
-            } else if (board[k][j] == 'B') {
-                break;
+        int l = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (c[s.charAt(i) - 'a'] < k) {
+                a = Math.max(a, longestSubstring(s.substring(l + 1, i), k));
+                l = i;
             }
         }
 
-        for (int k = i + 1; k <8; k++) {
-            if (board[k][j] == 'p') {
-                a++;
-                break;
-            } else if (board[k][j] == 'B') {
-                break;
-            }
-        }
-
-        for (int k = j - 1; k >= 0; k--) {
-            if (board[i][k] == 'p') {
-                a++;
-                break;
-            } else if (board[i][k] == 'B') {
-                break;
-            }
-        }
-
-        for (int k = i + 1; k < 8; k++) {
-            if (board[i][k] == 'p') {
-                a++;
-                break;
-            } else if (board[i][k] == 'B') {
-                break;
-            }
-        }
-
+        if (l < s.length())
+            a = Math.max(a, longestSubstring(s.substring(l + 1), k));
         return a;
+    }
+
+    private int find(String s, int k) {
+        if (s.length() == 0) return 0;
+
+        int[] c = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            c[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (c[i] > 0 && c[i] < k) return 0;
+        }
+        return s.length();
     }
 
 }
