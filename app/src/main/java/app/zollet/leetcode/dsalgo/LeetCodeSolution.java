1375b8069cb41e5c902a9babfc8aa8179cc7f837
abhilashdas <abhilashmyworld@gmail.com>
2020-11-28 20:34:25 +0530

    solve "Maximum Repeating Substring"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,30 +1,33 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.PriorityQueue;
-
 public class LeetCodeSolution {
 
     public void execute() {
+        int a = maxRepeating("aaa", "a");
     }
 
-    public int largestSumAfterKNegations(int[] A, int K) {
+    public int maxRepeating(String sequence, String word) {
+        if (word.length() > sequence.length()) return 0;
+        if (sequence.length() == 0 || word.length() == 0) return 0;
+        if (sequence.equals(word)) return 1;
 
-        PriorityQueue<Integer> treeMap = new PriorityQueue<>();
+        int t = 1;
 
-        int s = 0;
-        for (int i = 0; i < A.length; i++) {
-            treeMap.add(A[i]);
-            s += A[i];
+        int a = 0;
+
+        StringBuilder sb = new StringBuilder(sequence);
+        for (int i = 0; i <= sequence.length() - word.length(); i++) {
+            if (sb.substring(i, i+word.length()).equals(word)) {
+                int l = 1;
+                int j = i + word.length();
+                while (j + word.length() <= sequence.length() && sb.toString().substring(j, j + word.length()).equals(word)) {
+                    l++;
+                    j = j + word.length();
+                }
+                a = Math.max(a, l);
+            }
         }
-
-
-        for (int i = 0; i < K; i++) {
-            int v = treeMap.poll();
-            s = s - v + (-1 * v);
-            treeMap.add(-v);
-        }
-        return s;
+        return a;
     }
-
 
 }
