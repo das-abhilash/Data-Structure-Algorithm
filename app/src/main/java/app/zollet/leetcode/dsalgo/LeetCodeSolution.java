1988f53124812fab7fc4effc98297add1a260b14
abhilashdas <abhilashmyworld@gmail.com>
2020-11-28 16:33:55 +0530

    solve "Maximize Sum Of Array After K Negations"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,46 +1,30 @@
 package app.zollet.leetcode.dsalgo;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
-import java.util.Comparator;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int largestSumAfterKNegations(int[] A, int K) {
 
-        if (nums.length == 0) return nums;
-        TreeMap<Integer, Integer> map = new TreeMap<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer integer, Integer t1) {
-                return Integer.compare(integer, t1) * -1;
-            }
-        });
-        int[] a = new int[nums.length - k + 1];
-        int index = 0;
-        for (int i = 0; i < k && i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        PriorityQueue<Integer> treeMap = new PriorityQueue<>();
+
+        int s = 0;
+        for (int i = 0; i < A.length; i++) {
+            treeMap.add(A[i]);
+            s += A[i];
         }
 
-        a[index++] = map.firstKey();
 
-        for (int i = k; i < nums.length; i++) {
-            int v = map.getOrDefault(nums[i - k], 0);
-            if (v == 1)
-                map.remove(nums[i - k]);
-            else
-                map.put(nums[i - k], v - 1);
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            a[index++] = (map.firstKey());
+        for (int i = 0; i < K; i++) {
+            int v = treeMap.poll();
+            s = s - v + (-1 * v);
+            treeMap.add(-v);
         }
-
-        return a;
+        return s;
     }
+
 
 }
