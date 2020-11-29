10ff068f252c05b49c6b1f0cece48b11e25c5c57
abhilashdas <abhilashmyworld@gmail.com>
2020-11-29 16:18:12 +0530

    solve "Jump Game III"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -5,19 +5,27 @@ public class LeetCodeSolution {
     public void execute() {
     }
 
-    public int maximumWealth(int[][] accounts) {
+    public boolean canReach(int[] arr, int start) {
 
-        int s = 0;
+        return solve(arr, start);
 
-        for (int i = 0; i < accounts.length; i++) {
-            int ss = 0;
-            for (int j = 0; j < accounts[i].length; j++) {
-                ss += accounts[i][j];
-            }
-            s = Math.max(s, ss);
-        }
-        return s;
+
     }
 
+    private boolean solve(int[] arr, int index) {
+
+        if (arr[index] == 0) return true;
+        if (arr[index] == -1) return false;
+        int v1 = arr[index] + index;
+        int v2 = -arr[index] + index;
+
+        int temp = arr[index];
+        arr[index] = -1;
+
+        boolean a1 = (v1 < arr.length && v1 >= 0) && solve(arr, v1);
+        boolean a2 = (v2 < arr.length && v2 >= 0) && solve(arr, v2);
+        arr[index] = temp;
+        return a1 || a2;
+    }
 
 }
