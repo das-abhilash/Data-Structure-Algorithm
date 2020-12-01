9d07fbfd1ecff56d67a96925e18ef418b28c4ec5
abhilashdas <abhilashmyworld@gmail.com>
2020-12-01 21:43:22 +0530

    solve "Maximum Depth of Binary Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,31 +1,16 @@
 package app.zollet.leetcode.dsalgo;
 
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public boolean canReach(int[] arr, int start) {
-
-        return solve(arr, start);
-
-
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
-    private boolean solve(int[] arr, int index) {
-
-        if (arr[index] == 0) return true;
-        if (arr[index] == -1) return false;
-        int v1 = arr[index] + index;
-        int v2 = -arr[index] + index;
-
-        int temp = arr[index];
-        arr[index] = -1;
-
-        boolean a1 = (v1 < arr.length && v1 >= 0) && solve(arr, v1);
-        boolean a2 = (v2 < arr.length && v2 >= 0) && solve(arr, v2);
-        arr[index] = temp;
-        return a1 || a2;
-    }
 
 }
