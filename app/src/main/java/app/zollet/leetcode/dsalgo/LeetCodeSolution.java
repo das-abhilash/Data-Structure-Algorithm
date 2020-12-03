fbc6dd8067e374d6a478056d6be70b9bc97bd0fb
abhilashdas <abhilashmyworld@gmail.com>
2020-12-03 20:02:20 +0530

    solve "Increasing Order Search Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -2,39 +2,55 @@ package app.zollet.leetcode.dsalgo;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-import app.zollet.leetcode.dsalgo.util.ListNode;
+import app.zollet.leetcode.dsalgo.util.TreeNode;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    class Solution {
+    List<Integer> list = new ArrayList<>();
 
-        List<Integer> list;
+    public TreeNode increasingBST(TreeNode root) {
+        list = new ArrayList<>();
+        solve(root);
 
-        /**
-         * @param head The linked list's head.
-         *             Note that the head is guaranteed to be not null, so it contains at least one node.
-         */
-        public Solution(ListNode head) {
-            list = new ArrayList<>();
-            while (head != null) {
-                list.add(head.val);
-                head = head.next;
-            }
+        if (list.size() == 0) return null;
+        TreeNode treeNode = new TreeNode(list.get(0));
+
+        TreeNode cur = treeNode;
+        for (int i = 1; i < list.size(); i++) {
+            cur.right = new TreeNode(list.get(i));
+            cur = cur.right;
+
         }
-
-        /**
-         * Returns a random node's value.
-         */
-        public int getRandom() {
-            int r = new Random().nextInt(list.size());
-            return list.get(r);
-        }
+        return treeNode;
     }
 
+    private void solve(TreeNode root) {
+        if (root == null) return;
+        solve(root.left);
+        list.add(root.val);
+        solve(root.right);
+    }
+
+    /*private TreeNode solve(TreeNode node) {
+        if (node == null) return null;
+
+        TreeNode left = solve(node.left);
+        if (left == null) {
+            if (node.right == null) {
+                return node;
+            }
+            return node.right;
+        } else if (node.right == null) {
+            left.right = node;
+            return node;
+        } else {
+            left.right = node;
+            return node.right;
+        }
+    }*/
 
 }
