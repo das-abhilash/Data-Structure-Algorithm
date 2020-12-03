408990a482b094e94be8c5f8ba191181fce52533
abhilashdas <abhilashmyworld@gmail.com>
2020-12-03 09:24:16 +0530

    solve "Linked List Random Node"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,15 +1,39 @@
 package app.zollet.leetcode.dsalgo;
 
-import app.zollet.leetcode.dsalgo.util.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import app.zollet.leetcode.dsalgo.util.ListNode;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    class Solution {
+
+        List<Integer> list;
+
+        /**
+         * @param head The linked list's head.
+         *             Note that the head is guaranteed to be not null, so it contains at least one node.
+         */
+        public Solution(ListNode head) {
+            list = new ArrayList<>();
+            while (head != null) {
+                list.add(head.val);
+                head = head.next;
+            }
+        }
+
+        /**
+         * Returns a random node's value.
+         */
+        public int getRandom() {
+            int r = new Random().nextInt(list.size());
+            return list.get(r);
+        }
     }
 
 
