2cccda21aae267d62d712403bd6ea6c3bc0bdca2
abhilashdas <abhilashmyworld@gmail.com>
2020-12-07 15:52:35 +0530

    solve "Spiral Matrix II"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,57 +1,56 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class LeetCodeSolution {
 
     public void execute() {
+        int[][] g = generateMatrix(3);
     }
 
-    class Node {
-        public int val;
-        public Node left;
-        public Node right;
-        public Node next;
-
-        public Node() {
-        }
-
-        public Node(int _val) {
-            val = _val;
-        }
-
-        public Node(int _val, Node _left, Node _right, Node _next) {
-            val = _val;
-            left = _left;
-            right = _right;
-            next = _next;
-        }
-    }
-
-    ;
-
-    public Node connect(Node root) {
-        if (root == null) return null;
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            Node prev = null;
-            for (int i = 0; i < size; i++) {
-                Node pop = queue.poll();
-                if (pop.left != null) queue.add(pop.left);
-                if (pop.right != null) queue.add(pop.right);
-                if (i != 0) {
-                    prev.next = pop;
-                }
-                prev = pop;
+    public int[][] generateMatrix(int n) {
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = -1;
             }
-            prev.next = null;
         }
-        return root;
+
+        solveLR(grid, 0, 0, 1, n);
+        return grid;
     }
 
+    private void solveLR(int[][] grid, int x, int y, int v, int n) {
+        if (x < 0 || y < 0 || x >= n || y >= n || grid[x][y] != -1) return;
+
+        for (; y < n && grid[x][y] == -1; y++) {
+            grid[x][y] = v++;
+        }
+        solveTB(grid, x+1, y-1, v, n);
+    }
+
+    private void solveTB(int[][] grid, int x, int y, int v, int n) {
+        if (x < 0 || y < 0 || x >= n || y >= n || grid[x][y] != -1) return;
+
+        for (; x < n && grid[x][y] == -1; x++) {
+            grid[x][y] = v++;
+        }
+        solveRL(grid, x-1, y-1, v, n);
+    }
+
+    private void solveRL(int[][] grid, int x, int y, int v, int n) {
+        if (x < 0 || y < 0 || x >= n || y >= n || grid[x][y] != -1) return;
+
+        for (; y >= 0 && grid[x][y] == -1; y--) {
+            grid[x][y] = v++;
+        }
+        solveBT(grid, x-1, y+1, v, n);
+    }
+
+    private void solveBT(int[][] grid, int x, int y, int v, int n) {
+        if (x < 0 || y < 0 || x >= n || y >= n || grid[x][y] != -1) return;
+
+        for (; x >= 0 && grid[x][y] == -1; x--) {
+            grid[x][y] = v++;
+        }
+        solveLR(grid, x+1, y+1, v, n);
+    }
 }
