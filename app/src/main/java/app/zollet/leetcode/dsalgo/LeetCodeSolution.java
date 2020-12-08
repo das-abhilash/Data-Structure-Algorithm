31524ccab3d8cbc6a3d9681f1717478d375cab99
abhilashdas <abhilashmyworld@gmail.com>
2020-12-08 17:07:02 +0530

    solve "Complement of Base 10 Integer"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,26 +1,15 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = numPairsDivisibleBy60(new int[]{30, 20, 150, 100, 40});
+        int a = bitwiseComplement(10);
     }
 
-    public int numPairsDivisibleBy60(int[] time) {
-
-        int[] dp = new int[61];
-        Arrays.fill(dp, 0);
-        int a = 0;
-        for (int t : time) {
-            int r = t % 60;
-            if (dp[r == 0 ? 60 : 60 - r] != 0) {
-                a += dp[r == 0 ? 60 : 60 - r];
-            }
-            dp[r == 0 ? 60 :r]++;
-        }
-        return a;
+    public int bitwiseComplement(int N) {
+        if (N == 0) return 1;
+        int l = (int) (Math.log(N) / Math.log(2)) + 1;
+        return N ^ ((int) Math.pow(2,l) -1);
     }
 
 }
