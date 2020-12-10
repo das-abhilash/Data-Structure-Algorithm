8b57999f40665862f7e734933a67a69254040c85
abhilashdas <abhilashmyworld@gmail.com>
2020-12-10 22:55:48 +0530

    solve "Valid Mountain Array"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,68 +1,30 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        List<Integer> a = spiralOrder(new int[][]{
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        });
+
     }
 
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) return false;
 
+        int i = 1;
 
-        List<Integer> a = new ArrayList<>();
-        int l = 0;
-        int r = matrix[0].length;
-        int t = 0;
-        int b = matrix.length;
-
-
-        int x = 0;
-        int y = 0;
-        while (true) {
-
-            int sdf = 0;
-            for (; y < r; y++) {
-                a.add(matrix[x][y]);
-            }
-            y--;
-            x++;
-            t++;
-            if (l >= r || t >= b) break;
-            for (; x < b; x++) {
-                a.add(matrix[x][y]);
-            }
-            y--;
-            x--;
-            r--;
-            if (l >= r || t >= b) break;
-
-            for (; y >= l; y--) {
-                a.add(matrix[x][y]);
-            }
-            y++;
-            x--;
-            b--;
-
-            if (l >= r || t >= b) break;
-
-            for (; x >= t; x--) {
-                a.add(matrix[x][y]);
-            }
-            x++;
-            y++;
-            l++;
-            if (l >= r || t >= b) break;
-
+        for (; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                return false;
+            } else if (arr[i] < arr[i - 1])
+                break;
         }
+        if (i == 1 || i == arr.length) return false;
 
-        return a;
+        for (; i < arr.length; i++) {
+            if (arr[i] >= arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
