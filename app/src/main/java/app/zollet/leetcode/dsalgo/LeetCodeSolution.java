6e532e1c5f80f0aab3ff43eb55e32e84027182f2
abhilashdas <abhilashmyworld@gmail.com>
2020-12-13 08:09:16 +0530

    solve "Count of Matches in Tournament"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -3,36 +3,22 @@ package app.zollet.leetcode.dsalgo;
 public class LeetCodeSolution {
 
     public void execute() {
-        int[] a = getSumAbsoluteDifferences(new int[]{2, 3, 5});
+
     }
 
-    public int[] getSumAbsoluteDifferences(int[] nums) {
+    public int numberOfMatches(int n) {
+        if (n == 1) return 0;
+        int a = 0;
 
+        while (n > 1) {
 
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-
-        left[0] = nums[0];
-        right[nums.length - 1] = nums[nums.length - 1];
-
-        for (int i = 1; i < nums.length; i++) {
-            left[i] = left[i - 1] + nums[i];
-            right[nums.length - 1 - i] = right[nums.length - i] + nums[nums.length - 1 - i];
-        }
-
-        int[] a = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                a[i] = right[1] - (nums[i] * (nums.length - 1));
-            } else if (i == nums.length - 1) {
-                a[i] = (nums[i] * (nums.length - 1)) - left[nums.length - 2];
+            if (n % 2 == 0) {
+                a += n / 2;
+                n = n / 2;
             } else {
-                int r = right[i + 1] - nums[i] * (nums.length - i - 1);
-                int l = nums[i] * (i) - left[i - 1];
-                a[i] = r + l;
+                a += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
             }
-
         }
         return a;
     }
