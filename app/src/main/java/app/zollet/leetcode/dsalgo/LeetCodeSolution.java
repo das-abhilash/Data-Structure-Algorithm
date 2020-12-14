7b609a7dce1d7d70eed62cee31c72b221a69d54e
abhilashdas <abhilashmyworld@gmail.com>
2020-12-14 08:08:46 +0530

    solve "Smallest Subtree with all the Deepest Nodes"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,26 +1,47 @@
 package app.zollet.leetcode.dsalgo;
 
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+import app.zollet.leetcode.dsalgo.util.TreeNodeUtill;
+
 public class LeetCodeSolution {
 
     public void execute() {
-
+        TreeNode root = TreeNodeUtill.insertLevelOrder(new int[]{3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4}, new TreeNode(3), 0);
+        TreeNode a = subtreeWithAllDeepest(root);
     }
 
-    public int numberOfMatches(int n) {
-        if (n == 1) return 0;
-        int a = 0;
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        solve(root, 0);
+        return solve(root, 0).first;
+    }
 
-        while (n > 1) {
+    class Pair {
+        TreeNode first;
+        int second;
 
-            if (n % 2 == 0) {
-                a += n / 2;
-                n = n / 2;
-            } else {
-                a += (n - 1) / 2;
-                n = (n - 1) / 2 + 1;
-            }
+        public Pair(TreeNode first, int second) {
+            this.first = first;
+            this.second = second;
         }
-        return a;
+    }
+
+    private Pair solve(TreeNode node, int d) {
+        if (node == null) return new Pair(null, d);
+
+        Pair pL = solve(node.left, d + 1);
+        Pair pR = solve(node.right, d + 1);
+
+        if (pL.first == null && pR.first == null) return new Pair(node, d);
+        if (pL.first == null) return pR;
+        if (pR.first == null) return pL;
+
+        if (pL.second > pR.second) {
+            return pL;
+        } else if (pR.second > pL.second) {
+            return pR;
+        } else {
+            return new Pair(node, Math.max(pL.second, pR.second));
+        }
     }
 
 }
