47d70dacfc53039ac45da07c74bedc843222bc56
abhilashdas <abhilashmyworld@gmail.com>
2020-12-14 17:03:23 +0530

    solve "Palindrome Partitioning"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,47 +1,45 @@
 package app.zollet.leetcode.dsalgo;
 
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-import app.zollet.leetcode.dsalgo.util.TreeNodeUtill;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        TreeNode root = TreeNodeUtill.insertLevelOrder(new int[]{3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4}, new TreeNode(3), 0);
-        TreeNode a = subtreeWithAllDeepest(root);
+        List<List<String>> a = partition("aab");
     }
 
-    public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        solve(root, 0);
-        return solve(root, 0).first;
+    List<List<String>> an = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+
+        solve(s, new ArrayList<>());
+        return an;
     }
 
-    class Pair {
-        TreeNode first;
-        int second;
+    private void solve(String s, List<String> a) {
 
-        public Pair(TreeNode first, int second) {
-            this.first = first;
-            this.second = second;
+        if (s.length() == 0) {
+            List<String> l = new ArrayList<>(a);
+            an.add(l);
+            return;
         }
+
+        for (int i = 0; i < s.length(); i++) {
+
+            String ss = s.substring(0, i+1);
+            if (isPalindrome(ss)) {
+                a.add(ss);
+                solve(s.substring(i+1), a);
+                a.remove(a.size() - 1);
+            }
+        }
+
     }
 
-    private Pair solve(TreeNode node, int d) {
-        if (node == null) return new Pair(null, d);
-
-        Pair pL = solve(node.left, d + 1);
-        Pair pR = solve(node.right, d + 1);
-
-        if (pL.first == null && pR.first == null) return new Pair(node, d);
-        if (pL.first == null) return pR;
-        if (pR.first == null) return pL;
-
-        if (pL.second > pR.second) {
-            return pL;
-        } else if (pR.second > pL.second) {
-            return pR;
-        } else {
-            return new Pair(node, Math.max(pL.second, pR.second));
-        }
+    private boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString().equals(s);
     }
 
 }
