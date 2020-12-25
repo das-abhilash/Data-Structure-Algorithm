3ce1755fb379ebe2b9b78d881b8eaa6b6ffdcefe
abhilashdas <abhilashmyworld@gmail.com>
2020-12-25 22:08:07 +0530

    solve "Diagonal Traverse"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,42 +1,85 @@
 package app.zollet.leetcode.dsalgo;
 
-import app.zollet.leetcode.dsalgo.util.ListNode;
-import app.zollet.leetcode.dsalgo.util.ListNodeUtil;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        ListNodeUtil u = new ListNodeUtil();
-        u.push(4);
-        u.push(3);
-        u.push(2);
-        u.push(1);
-        ListNode a = swapPairs(u.head);
+
     }
 
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode cur = head;
+    public int[] findDiagonalOrder(int[][] matrix) {
 
-        ListNode listNode = cur.next;
-
-        ListNode last = null;
-        while (cur != null && cur.next != null) {
-
-            ListNode nextnext = cur.next.next;
-
-            ListNode next = cur.next;
-
-            cur.next = nextnext;
-            next.next = cur;
-            if (last != null) {
-                last.next = next;
-            }
-            last = cur;
-            cur = cur.next;
-
+        // Check for empty matrices
+        if (matrix == null || matrix.length == 0) {
+            return new int[0];
         }
-        return listNode;
+
+        // Variables to track the size of the matrix
+        int N = matrix.length;
+        int M = matrix[0].length;
+
+        // Incides that will help us progress through
+        // the matrix, one element at a time.
+        int row = 0, column = 0;
+
+        // As explained in the article, this is the variable
+        // that helps us keep track of what direction we are
+        // processing the current diaonal
+        int direction = 1;
+
+        // The final result array
+        int[] result = new int[N * M];
+        int r = 0;
+
+        // The uber while loop which will help us iterate over all
+        // the elements in the array.
+        while (row < N && column < M) {
+
+            // First and foremost, add the current element to
+            // the result matrix.
+            result[r++] = matrix[row][column];
+
+            // Move along in the current diagonal depending upon
+            // the current direction.[i, j] -> [i - 1, j + 1] if
+            // going up and [i, j] -> [i + 1][j - 1] if going down.
+            int new_row = row + (direction == 1 ? -1 : 1);
+            int new_column = column + (direction == 1 ? 1 : -1);
+
+            // Checking if the next element in the diagonal is within the
+            // bounds of the matrix or not. If it's not within the bounds,
+            // we have to find the next head.
+            if (new_row < 0 || new_row == N || new_column < 0 || new_column == M) {
+
+                // If the current diagonal was going in the upwards
+                // direction.
+                if (direction == 1) {
+
+                    // For an upwards going diagonal having [i, j] as its tail
+                    // If [i, j + 1] is within bounds, then it becomes
+                    // the next head. Otherwise, the element directly below
+                    // i.e. the element [i + 1, j] becomes the next head
+                    row += (column == M - 1 ? 1 : 0);
+                    column += (column < M - 1 ? 1 : 0);
+
+                } else {
+
+                    // For a downwards going diagonal having [i, j] as its tail
+                    // if [i + 1, j] is within bounds, then it becomes
+                    // the next head. Otherwise, the element directly below
+                    // i.e. the element [i, j + 1] becomes the next head
+                    column += (row == N - 1 ? 1 : 0);
+                    row += (row < N - 1 ? 1 : 0);
+                }
+
+                // Flip the direction
+                direction = 1 - direction;
+
+            } else {
+
+                row = new_row;
+                column = new_column;
+            }
+        }
+        return result;
     }
 
 }
