bb998d4f45b6f7519495205a4379c2fa9f503ee4
abhilashdas <abhilashmyworld@gmail.com>
2020-12-28 15:25:43 +0530

    solve "Jump Game IV"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,12 +1,10 @@
 package app.zollet.leetcode.dsalgo;
 
-import android.util.Log;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class LeetCodeSolution {
@@ -16,42 +14,44 @@ public class LeetCodeSolution {
     }
 
     public int minJumps(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Map<Integer, List<Integer>> map2 = new HashMap<>();
+        Map<Integer, Set<Integer>> map2 = new HashMap<>();
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
-            List<Integer> l = map2.getOrDefault(arr[i], new ArrayList<>());
-            l.add(i);
-            map2.put(arr[i], l);
+            map2.computeIfAbsent(arr[i], x -> new HashSet<>()).add(i);
         }
 
-        return solve(arr, 0, map, 0, set, map2);
-    }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
 
-    private int solve(int[] arr, int i, Map<Integer, Integer> map, int c, Set<Integer> set, Map<Integer, List<Integer>> map2) {
-        if (i == arr.length - 1) {
-            Log.d("mehbla", "reached");
-            return c;
-        }
-        if (i < 0 || i >= arr.length || set.contains(i)) return Integer.MAX_VALUE;
+        int a = 0;
+        int f = arr.length - 1;
+        while (!queue.isEmpty()) {
+            int s = queue.size();
+            for (int i = 0; i < s; i++) {
+                int x = queue.poll();
+                if (x == f) return a;
+                Set<Integer> se = map2.get(arr[x]);
+                map2.put(arr[x], new HashSet<>());
+                if (x != 0) {
+                    if (!set.contains(x - 1)) {
+                        queue.add(x - 1);
+                        set.add(x - 1);
+                    }
+                }
+                if (!set.contains(x + 1)) {
+                    queue.add(x + 1);
+                    set.add(x + 1);
+                }
 
-//        if (map.containsKey(i)) return map.get(i);
-
-        set.add(i);
-        Log.d("mehbla", arr[i] + "");
-        int a = Math.min(solve(arr, i + 1, map, c + 1, set, map2), solve(arr, i - 1, map, c + 1, set, map2));
-
-        List<Integer> l = map2.getOrDefault(arr[i], new ArrayList<>());
-        if (l.size() > 1) {
-            for (int j = 0; j < l.size(); j++) {
-                if (l.get(j) != i) {
-                    a = Math.min(solve(arr, l.get(j), map, c + 1, set, map2), a);
+                for (int j : se) {
+                    if (!set.contains(j)) {
+                        set.add(j);
+                        queue.add(j);
+                    }
                 }
             }
+            a++;
         }
-
-        set.remove(i);
-        map.put(i, a);
         return a;
     }
 
