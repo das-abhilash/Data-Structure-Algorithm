043882707ef16d3074174f94c7df565f4fcd9dee
abhilashdas <abhilashmyworld@gmail.com>
2020-12-31 23:19:51 +0530

    solve "Largest Rectangle in Histogram"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,9 +1,34 @@
 package app.zollet.leetcode.dsalgo;
 
+import java.util.Stack;
+
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
+    public int largestRectangleArea(int[] heights) {
+
+        if (heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+
+        int a = 0;
+        for (int i = 0; i <= heights.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+                continue;
+            }
+            while (i == heights.length || heights[stack.peek()] <= heights[i]) {
+                int p = stack.pop();
+                if (stack.isEmpty()) {
+                    a = Math.max(a, heights[p] * i);
+                    break;
+                } else
+                    a = Math.max(a, heights[p] * (i - stack.peek() +1));
+            }
+            stack.push(i);
+        }
+        return a;
+    }
 
 }
