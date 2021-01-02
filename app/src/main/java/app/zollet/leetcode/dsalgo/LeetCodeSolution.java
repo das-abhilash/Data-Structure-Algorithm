da0e1fdbcc0d98da18bfdd620349f334cbdafd9d
abhilashdas <abhilashmyworld@gmail.com>
2021-01-02 14:59:17 +0530

    solve "Find a Corresponding Node of a Binary Tree in a Clone of That Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,40 +1,20 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import app.zollet.leetcode.dsalgo.util.TreeNode;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        List<String> w = new ArrayList<>();
-        w.add("apple");
-        w.add("pen");
-        boolean a = wordBreak("applepenapple", w);
     }
 
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) return null;
+        if (original.val == target.val) return cloned;
 
-        Set<String> invalid = new HashSet<>();
-        Set<String> set = new HashSet<>(wordDict);
-        if (set.contains(s)) return true;
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        TreeNode right = getTargetCopy(original.right, cloned.right, target);
 
-        return solve(s, 0, set, invalid);
-
-    }
-
-    private boolean solve(String s, int i, Set<String> wordDict, Set<String> set) {
-
-        if (set.contains(s)) return false;
-        if (wordDict.contains(s)) return true;
-        for (int k = i + 1; k < s.length(); k++) {
-            String ss = s.substring(i, k);
-            if (wordDict.contains(ss)) {
-                if (solve(s.substring(k), 0, wordDict, set)) return true;
-            }
-        }
-        set.add(s);
-        return false;
+        if (left != null) return left;
+        return right;
     }
 }
