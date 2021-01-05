4418246e6ca94f3a23610402fa34f67f81ca3594
abhilashdas <abhilashmyworld@gmail.com>
2021-01-06 00:01:18 +0530

    clean slate

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -8,35 +8,6 @@ public class LeetCodeSolution {
 
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        ListNode dummy = new ListNode();
-
-        ListNode r = l2;
-
-        dummy.next = l1;
-        ListNode cur = l1;
-        ListNode prev = dummy;
-
-        while (cur != null && r != null) {
-            if (cur.val < r.val) {
-                ListNode temp = cur;
-                cur = cur.next;
-                prev = temp;
-            } else {
-                ListNode temp = r.next;
-                prev.next = r;
-                prev = prev.next;
-                r.next = cur;
-                r = temp;
-            }
-        }
-
-        if (r!=null){
-            prev.next = r;
-        }
-
-        return dummy.next;
-    }
 
 }
