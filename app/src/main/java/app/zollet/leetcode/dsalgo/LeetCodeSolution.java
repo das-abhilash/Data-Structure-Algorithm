99c6206d24e1c7d0576afb08b159dd620773856f
abhilashdas <abhilashmyworld@gmail.com>
2021-01-08 16:06:33 +0530

    solve "Check If Two String Arrays are Equivalent"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,36 +1,23 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = lengthOfLongestSubstring("aab");
     }
 
-    public int lengthOfLongestSubstring(String s) {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
 
-        if (s.length() == 0) return 0;
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
-        Map<Character, Integer> map = new HashMap<>();
-        int a = -1;
-        int in = 0;
-        map.put(s.charAt(0), 0);
-
-        for (int i = 1; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (map.containsKey(c)) {
-                int ind = map.get(c);
-                if (ind >= in) {
-                    a = Math.max(a, i - in);
-                    in = ind + 1;
-                }
-            }
-            map.put(c, i);
+        for (String s : word1) {
+            sb1.append(s);
         }
-        a = Math.max(a, s.length() -1 - in);
-        return a;
+
+        for (String s : word2) {
+            sb2.append(s);
+        }
+        return sb1.toString().equals(sb2.toString());
     }
+
 }
