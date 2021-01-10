28dc7f4eac78b756283eb9874acbcbd3a11535c4
abhilashdas <abhilashmyworld@gmail.com>
2021-01-10 08:43:53 +0530

    solve "Decode XORed Array"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -5,4 +5,16 @@ public class LeetCodeSolution {
     public void execute() {
     }
 
+    public int[] decode(int[] encoded, int first) {
+
+        if (encoded.length == 0) return new int[]{first};
+
+        int[] a = new int[encoded.length + 1];
+        a[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            a[i + 1] = encoded[i] ^ a[i];
+        }
+        return a;
+    }
+
 }
