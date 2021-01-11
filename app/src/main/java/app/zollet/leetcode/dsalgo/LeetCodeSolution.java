89e2e4eb305c0080dec70f7b0ac9e7aec196c970
abhilashdas <abhilashmyworld@gmail.com>
2021-01-11 21:56:07 +0530

    solve "Merge Sorted Array"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -3,18 +3,43 @@ package app.zollet.leetcode.dsalgo;
 public class LeetCodeSolution {
 
     public void execute() {
+        merge(new int[]{2, 5, 6, 0, 0, 0}, 3, new int[]{1, 2, 3}, 3);
     }
 
-    public int[] decode(int[] encoded, int first) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        if (encoded.length == 0) return new int[]{first};
+        int l = 0;
+        int r = 0;
 
-        int[] a = new int[encoded.length + 1];
-        a[0] = first;
-        for (int i = 0; i < encoded.length; i++) {
-            a[i + 1] = encoded[i] ^ a[i];
+        while (m <= nums1.length && l<m && r < n) {
+
+            int f = nums1[l];
+            int s = nums2[r];
+
+            if (f <= s) {
+                l++;
+            } else {
+                shift(nums1, l + 1, m);
+                nums1[l] = nums2[r];
+                r++;
+                l++;
+                m++;
+            }
+            int ssfd = 0;
         }
-        return a;
+
+        while (r < n) {
+            nums1[l++] = nums2[r++];
+        }
+        int aa = 0;
+    }
+
+    private void shift(int[] a, int i, int m) {
+
+        for (int j = a.length - 1; j >= i; j--) {
+            a[j] = a[j - 1];
+        }
+
     }
 
 }
