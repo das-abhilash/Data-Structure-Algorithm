f80dc56f9b0393d7c181c1aa404352ec275d874f
abhilashdas <abhilashmyworld@gmail.com>
2021-01-17 22:22:32 +0530

    solve "Count Sorted Vowel Strings"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -7,12 +7,28 @@ import app.zollet.leetcode.dsalgo.util.ListNode;
 public class LeetCodeSolution {
 
     public void execute() {
+        int aa = countVowelStrings(5);
     }
 
-    public int findKthLargest(int[] nums, int k) {
+    public int countVowelStrings(int n) {
 
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        if (n == 1) return 5;
+        int[] a = new int[5];
+        Arrays.fill(a, 1);
 
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 5; j++) {
+                int m = 0;
+                for (int k = j; k < 5; k++) {
+                    m += a[k];
+                }
+                a[j] = m;
+            }
+        }
+        int aa = 0;
+        for (int i = 0; i < 5; i++) {
+            aa += a[i];
+        }
+        return aa;
     }
 }
