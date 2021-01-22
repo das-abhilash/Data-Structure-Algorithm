5c976812b35cfae5845caeed58d379bc924a0bf7
abhilashdas <abhilashmyworld@gmail.com>
2021-01-22 20:28:07 +0530

    solve "clean slate"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -14,29 +14,6 @@ public class LeetCodeSolution {
     public void execute() {
     }
 
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
 
-        Set<Character> ss = new HashSet<>();
-        ss.add(']');
-        ss.add(')');
-        ss.add('}');
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (ss.contains(c)) {
-                if (stack.isEmpty()) return false;
-                if (c == ')' && stack.peek() != '(') return false;
-                if (c == '}' && stack.peek() != '{') return false;
-                if (c == ']' && stack.peek() != '[') return false;
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
-
-        }
-        return stack.isEmpty();
-    }
 
 }
