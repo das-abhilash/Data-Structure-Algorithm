66326d1cc8939490b897fa9e8ab5f0728756f573
abhilashdas <abhilashmyworld@gmail.com>
2021-02-07 08:26:24 +0530

    solve "Check if Array Is Sorted and Rotated"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -3,19 +3,36 @@ package app.zollet.leetcode.dsalgo;
 public class LeetCodeSolution {
 
     public void execute() {
+        boolean a = check(new int[]{2, 1, 3, 4});
     }
 
-    public int hammingWeight(int n) {
+    public boolean check(int[] nums) {
 
-        String b = Integer.toBinaryString(n);
+        int i = 0;
+        int max = nums[0];
+        int min = nums[0];
 
-        int a = 0;
-        for (int i = 0; i < b.length(); i++) {
-            if (b.charAt(i) == '1') {
-                a++;
+        while (i < nums.length - 1 && nums[i] <= nums[i + 1]) {
+            max = nums[i + 1];
+            min = Math.min(min,nums[i]);
+            i++;
+        }
+
+        if (i == nums.length - 1) return true;
+        i++;
+        while (i < nums.length - 1 && nums[i] <= nums[i + 1]) {
+            if (nums[i + 1] > max) return false;
+            if (nums[i] > min) return false;
+            i++;
+        }
+        if (i == nums.length - 1) return true;
+        if (i == 0) {
+            while (i < nums.length - 1 && nums[i] >= nums[i + 1]) {
+                i++;
             }
         }
-        return a;
+        return i == nums.length - 1;
+
     }
 
 }
