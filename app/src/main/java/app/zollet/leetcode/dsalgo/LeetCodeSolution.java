8b18a5b2d0290cae3861f3ff1855f4b337fc293a
abhilashdas <abhilashmyworld@gmail.com>
2021-02-11 23:42:53 +0530

    solve "Number of Operations to Make Network Connected"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -5,20 +5,56 @@ import java.util.Arrays;
 public class LeetCodeSolution {
 
 
+    int answer = 0;
+
     public void execute() {
-        boolean sdf = isAnagram("abc", "cba");
     }
 
-    public boolean isAnagram(String s, String t) {
+    private int find(int from, int[] root) {
+        if (root[from] == -1) return from;
 
-        if (s.length() != t.length()) return false;
+        int parent = find(root[from], root);
+        root[from] = parent;
+        return parent;
+    }
 
-        char[] ss = s.toCharArray();
-        char[] tt = t.toCharArray();
+    private void union(int from, int to, int[] root, int[] rank) {
 
-        Arrays.sort(ss);
-        Arrays.sort(tt);
-        return new String(ss).equals(new String(tt));
+        int fromRoot = find(from, root);
+        int toRoot = find(to, root);
+
+        if (fromRoot == toRoot) return;
+        int fromRank = rank[fromRoot];
+        int toRank = rank[toRoot];
+
+        if (fromRank > toRank) {
+            root[toRoot] = fromRoot;
+        } else if (fromRank < toRank) {
+            root[fromRoot] = toRoot;
+        } else {
+            root[fromRoot] = toRoot;
+            rank[fromRoot] = rank[fromRoot] + 1;
+        }
+        answer--;
+    }
+
+    public int makeConnected(int n, int[][] connections) {
+
+        if (connections.length < n - 1) return -1;
+        answer = n;
+        int[] t = new int[n];
+        int[] r = new int[n];
+
+        Arrays.fill(t, -1);
+        Arrays.fill(r, 1);
+
+        int b = 0;
+
+        for (int i = 0; i < connections.length; i++) {
+            union(connections[i][0], connections[i][1], t, r);
+        }
+
+        return answer - 1;
     }
 
 }
