de80dd171a16ce54cf623e1968bd3fc15a9da659
abhilashdas <abhilashmyworld@gmail.com>
2021-02-11 21:48:36 +0530

    solve "Valid Anagram"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,56 +1,24 @@
 package app.zollet.leetcode.dsalgo;
 
+import java.util.Arrays;
+
 public class LeetCodeSolution {
 
 
     public void execute() {
+        boolean sdf = isAnagram("abc", "cba");
     }
 
-    public Node copyRandomList(Node head) {
+    public boolean isAnagram(String s, String t) {
 
-        Node cur = head;
+        if (s.length() != t.length()) return false;
 
-        while (cur != null) {
-            Node temp = cur.next;
-            Node node = new Node(cur.val);
-            cur.next = node;
-            node.next = temp;
-            cur = temp;
-        }
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
 
-
-        cur = head;
-        while (cur != null) {
-            Node next = cur.next;
-            Node random = cur.random;
-            if (random != null)
-                next.random = random.next;
-            cur = cur.next.next;
-        }
-        cur = head;
-        Node dummy = new Node(-1);
-        Node curr = dummy;
-        while (cur != null) {
-            Node temp = cur.next.next;
-
-            curr.next = cur.next;
-            curr = curr.next;
-            cur.next = temp;
-            cur = temp;
-        }
-        return dummy.next;
-    }
-
-    class Node {
-        int val;
-        Node next;
-        Node random;
-
-        public Node(int val) {
-            this.val = val;
-            this.next = null;
-            this.random = null;
-        }
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        return new String(ss).equals(new String(tt));
     }
 
 }
