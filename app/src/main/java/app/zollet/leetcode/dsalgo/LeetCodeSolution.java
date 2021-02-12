653ca4971e47c44a11f2213fc7ac9649533b88a1
abhilashdas <abhilashmyworld@gmail.com>
2021-02-12 19:53:19 +0530

    solve "Number of Steps to Reduce a Number to Zero"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,60 +1,23 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
-
-
-    int answer = 0;
 
     public void execute() {
     }
 
-    private int find(int from, int[] root) {
-        if (root[from] == -1) return from;
+    public int numberOfSteps(int num) {
 
-        int parent = find(root[from], root);
-        root[from] = parent;
-        return parent;
-    }
+        int a = 0;
 
-    private void union(int from, int to, int[] root, int[] rank) {
-
-        int fromRoot = find(from, root);
-        int toRoot = find(to, root);
-
-        if (fromRoot == toRoot) return;
-        int fromRank = rank[fromRoot];
-        int toRank = rank[toRoot];
-
-        if (fromRank > toRank) {
-            root[toRoot] = fromRoot;
-        } else if (fromRank < toRank) {
-            root[fromRoot] = toRoot;
-        } else {
-            root[fromRoot] = toRoot;
-            rank[fromRoot] = rank[fromRoot] + 1;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num--;
+            }
+            a++;
         }
-        answer--;
-    }
-
-    public int makeConnected(int n, int[][] connections) {
-
-        if (connections.length < n - 1) return -1;
-        answer = n;
-        int[] t = new int[n];
-        int[] r = new int[n];
-
-        Arrays.fill(t, -1);
-        Arrays.fill(r, 1);
-
-        int b = 0;
-
-        for (int i = 0; i < connections.length; i++) {
-            union(connections[i][0], connections[i][1], t, r);
-        }
-
-        return answer - 1;
+        return a;
     }
 
 }
