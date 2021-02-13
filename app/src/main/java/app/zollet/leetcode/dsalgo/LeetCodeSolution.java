c18c0b4d120bfbbf1afc5cfaad6b9e5d2323225d
abhilashdas <abhilashmyworld@gmail.com>
2021-02-13 22:04:03 +0530

    solve "Shortest Path in Binary Matrix" (TLE)

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -2,22 +2,39 @@ package app.zollet.leetcode.dsalgo;
 
 public class LeetCodeSolution {
 
+
+
+
     public void execute() {
     }
+    int a = Integer.MAX_VALUE;
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid[0][0] == 1) return -1;
+        bfs(grid, 0, 0, 0);
+        return a == Integer.MAX_VALUE ? -1 : a;
+    }
 
-    public int numberOfSteps(int num) {
+    private void bfs(int[][] grid, int x, int y, int d) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == -1 || grid[x][y] == 1)
+            return;
 
-        int a = 0;
-
-        while (num != 0) {
-            if (num % 2 == 0) {
-                num = num / 2;
-            } else {
-                num--;
-            }
-            a++;
+        if (x == grid.length - 1 && y == grid[0].length - 1) {
+            a = Math.min(a, d + 1);
+            return;
         }
-        return a;
+
+        grid[x][y] = -1;
+
+        bfs(grid, x + 1, y, d + 1);
+        bfs(grid, x + 1, y - 1, d + 1);
+        bfs(grid, x + 1, y + 1, d + 1);
+        bfs(grid, x - 1, y + 1, d + 1);
+        bfs(grid, x - 1, y + 1, d + 1);
+        bfs(grid, x - 1, y + 1, d + 1);
+
+        bfs(grid, x, y - 1, d + 1);
+        bfs(grid, x, y + 1, d + 1);
+        grid[x][y] = 0;
     }
 
 }
