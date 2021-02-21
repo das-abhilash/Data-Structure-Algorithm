6bc72c8ddf59a1b94dc9dd90a841e599dce14c5a
abhilashdas <abhilashmyworld@gmail.com>
2021-02-21 08:37:48 +0530

    solve "Minimum Number of Operations to Move All Balls to Each Box"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -5,20 +5,30 @@ public class LeetCodeSolution {
     public void execute() {
     }
 
-    public String mergeAlternately(String word1, String word2) {
+    public int[] minOperations(String boxes) {
 
-        StringBuilder a = new StringBuilder();
-        int i = 0;
-        int j = 0;
-        while (i < word1.length() && j < word2.length()) {
+        int[] a = new int[boxes.length()];
 
-            a.append(word1.charAt(i++));
-            a.append(word2.charAt(j++));
+        for (int i = 0; i < boxes.length(); i++) {
+            int c = 0;
+            int s = 0;
+            for (int j = i - 1; j > 0; j--) {
+                s++;
+                if (boxes.charAt(j) == '1') {
+                    c += s;
+                }
+            }
+
+            s = 0;
+            for (int j = i + 1; j < boxes.length(); j++) {
+                s++;
+                if (boxes.charAt(j) == '1') {
+                    c += s;
+                }
+            }
+            a[i] = c;
         }
-
-        a.append(word1.substring(i));
-        a.append(word2.substring(j));
-        return a.toString();
+        return a;
     }
 
 }
