b286b0c5685aaa1fc8da50fa729221c085873ffd
abhilashdas <abhilashmyworld@gmail.com>
2021-02-21 08:24:52 +0530

    solve "Merge Strings Alternately"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,41 +1,24 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = romanToInt("MCMXCIV");
     }
 
-    public int romanToInt(String s) {
+    public String mergeAlternately(String word1, String word2) {
 
-        Map<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V", 5);
-        map.put("X", 10);
-        map.put("L", 50);
-        map.put("C", 100);
-        map.put("D", 500);
-        map.put("M", 1000);
-        map.put("IV", 4);
-        map.put("IX", 9);
-        map.put("XL", 40);
-        map.put("XC", 90);
-        map.put("CD", 400);
-        map.put("CM", 900);
+        StringBuilder a = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() && j < word2.length()) {
 
-        int a = 0;
-        for (int i = 0; i < s.length(); i++) {
-
-            if (i < s.length() - 1 && map.containsKey(s.substring(i, i + 2))) {
-                a += map.get(s.substring(i, i + 2));
-                i++;
-            } else {
-                a += map.get(String.valueOf(s.charAt(i)));
-            }
+            a.append(word1.charAt(i++));
+            a.append(word2.charAt(j++));
         }
-        return a;
+
+        a.append(word1.substring(i));
+        a.append(word2.substring(j));
+        return a.toString();
     }
+
 }
