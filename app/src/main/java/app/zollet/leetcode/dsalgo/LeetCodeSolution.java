3a926a4c47f98ce431bbaf70e347157b20a851a7
abhilashdas <abhilashmyworld@gmail.com>
2021-03-02 16:32:35 +0530

    solve "Set Mismatch"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,22 +1,35 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LeetCodeSolution {
 
     public void execute() {
+        int[] a = findErrorNums(new int[]{1, 2, 3, 4, 1});
     }
 
-    public int distributeCandies(int[] candyType) {
+    public int[] findErrorNums(int[] nums) {
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-
-        for (int i = 0; i < candyType.length; i++) {
-            map.put(candyType[i], map.getOrDefault(candyType[i], 0) + 1);
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set2.add(i + 1);
         }
 
-        return Math.min(map.size(), candyType.length / 2);
+        int[] out = new int[2];
 
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                out[0] = nums[i];
+            } else {
+                set.add(nums[i]);
+                set2.remove(nums[i]);
+            }
+        }
+
+        out[1] = set2.iterator().next();
+        return out;
     }
 
 }
