569d07c447bd00dad93f254ab1c4610af622a71f
abhilashdas <abhilashmyworld@gmail.com>
2021-03-03 23:09:44 +0530

    solve "Missing Number"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,35 +1,22 @@
 package app.zollet.leetcode.dsalgo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        int[] a = findErrorNums(new int[]{1, 2, 3, 4, 1});
+        int a = missingNumber(new int[]{1, 2, 3, 4, 5});
     }
 
-    public int[] findErrorNums(int[] nums) {
 
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            set2.add(i + 1);
-        }
-
-        int[] out = new int[2];
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                out[0] = nums[i];
-            } else {
-                set.add(nums[i]);
-                set2.remove(nums[i]);
-            }
+            if (nums[i] != i)
+                return i;
         }
-
-        out[1] = set2.iterator().next();
-        return out;
+        return nums.length;
     }
 
 }
