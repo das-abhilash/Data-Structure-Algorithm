95af813b3282244f645d7c7a76344caf1521f988
abhilashdas <abhilashmyworld@gmail.com>
2021-03-06 17:51:09 +0530

    clean slate

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,41 +1,10 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public List<Double> averageOfLevels(TreeNode root) {
-        if (root == null) return Collections.emptyList();
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Double> a = new ArrayList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            long sum = 0;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode n = queue.poll();
-                sum = sum + n.val;
-                if (n.left != null)
-                    queue.add(n.left);
-                if (n.right != null)
-                    queue.add(n.right);
-            }
-            double avg = (double) sum / size;
-            a.add(avg);
-        }
-        return a;
-    }
 }
