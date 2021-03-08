d4f4d177a40948c496421d9b4d8a34677b040e96
abhilashdas <abhilashmyworld@gmail.com>
2021-03-08 21:06:40 +0530

    solve "Remove Palindromic Subsequences"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,46 +1,15 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-
-    class MyHashMap {
-
-        int[] array;
-
-        /**
-         * Initialize your data structure here.
-         */
-        public MyHashMap() {
-            array = new int[1000000];
-            Arrays.fill(array, -1);
-        }
-
-        /**
-         * value will always be non-negative.
-         */
-        public void put(int key, int value) {
-            array[key] = value;
-        }
-
-        /**
-         * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-         */
-        public int get(int key) {
-            return array[key];
-        }
-
-        /**
-         * Removes the mapping of the specified value key if this map contains a mapping for the key
-         */
-        public void remove(int key) {
-            array[key] = -1;
-        }
+    public int removePalindromeSub(String s) {
+        if(s.length() == 0) return 0;
+        if(s.equals(new StringBuilder(s).reverse().toString())) return 1;
+        return 2;
     }
 
 }
