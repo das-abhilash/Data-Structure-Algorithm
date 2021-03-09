3f26de281494bbe93404933f42ee3ee55f3b85ac
abhilashdas <abhilashmyworld@gmail.com>
2021-03-09 22:14:44 +0530

    solve "Add One Row to Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,15 +1,56 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public int removePalindromeSub(String s) {
-        if(s.length() == 0) return 0;
-        if(s.equals(new StringBuilder(s).reverse().toString())) return 1;
-        return 2;
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+
+        if (d == 1) {
+            TreeNode treeNode = new TreeNode(v);
+            treeNode.left = root;
+            return treeNode;
+        }
+
+        TreeNode cur = root;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int de = 0;
+        while (!q.isEmpty()) {
+
+            de++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+
+                if (d == de - 1) {
+
+                    TreeNode left = node.left;
+                    TreeNode right = node.right;
+                    node.left = new TreeNode(v);
+                    node.right = new TreeNode(v);
+
+                    node.left.left = left;
+                    node.right.right = right;
+
+                } else {
+                    if (node.left != null)
+                        q.add(node.left);
+                    if (node.right != null)
+                        q.add(node.right);
+                }
+            }
+            if (d == de - 1) break;
+        }
+        return cur;
     }
 
 }
