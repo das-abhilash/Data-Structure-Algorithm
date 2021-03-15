679d1a89c1c607e3ae5d5ded42a0d9841012d59e
abhilashdas <abhilashmyworld@gmail.com>
2021-03-15 15:14:01 +0530

    solve "Encode and Decode TinyURL" (not optimized)(hacky solution)

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,37 +1,23 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import app.zollet.leetcode.dsalgo.util.ListNode;
-
 public class LeetCodeSolution {
 
 
     public void execute() {
     }
 
-    public ListNode swapNodes(ListNode head, int k) {
-        List<Integer> list = new ArrayList<>();
-        ListNode cur = head;
-        while (cur != null) {
-            list.add(cur.val);
-            cur = cur.next;
+    public class Codec {
+
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            return new StringBuilder(longUrl).reverse().toString();
         }
 
-        int temp = list.get(k-1);
-        list.set(k-1, list.get(list.size() - k));
-        list.set(list.size() - k, temp);
-
-        ListNode dummy = new ListNode();
-        ListNode cu = dummy;
-
-        for (int i = 0; i < list.size(); i++) {
-            cu.next = new ListNode(list.get(i));
-            cu = cu.next;
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return new StringBuilder(shortUrl).reverse().toString();
         }
-        return dummy.next;
     }
 
 }
