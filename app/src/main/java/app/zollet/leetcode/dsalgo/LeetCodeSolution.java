ea7191244f480dafc721e180835515d4847dd00d
Abhilash Das <abhilashmyworld@gmail.com>
2021-03-27 13:14:20 +0530

    solve "word subset"(TLE)

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,50 +1,72 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class LeetCodeSolution {
 
 
     public void execute() {
-
+        List<String> a = wordSubsets(new String[]{"amazon", "apple", "facebook", "google", "leetcode"}, new String[]{"e", "o"});
     }
 
-    public int[] advantageCount(int[] A, int[] B) {
-
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-
+    public List<String> wordSubsets(String[] A, String[] B) {
+        List<int[]> a = new ArrayList<>();
+        Set<String> answer = new HashSet<>();
         for (int i = 0; i < A.length; i++) {
-            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
+            String s = A[i];
+
+            int[] z = new int[26];
+            Arrays.fill(z, 0);
+            for (int j = 0; j < s.length(); j++) {
+                z[s.charAt(j) - 'a']++;
+            }
+            a.add(z);
         }
 
-        int[] a = new int[A.length];
+        List<int[]> b = new ArrayList<>();
 
         for (int i = 0; i < B.length; i++) {
-            if (map.higherKey(B[i]) != null) {
-                int k = map.higherKey(B[i]);
-                a[i] = k;
-                if (map.get(k) == 1)
-                    map.remove(k);
-                else
-                    map.put(k, map.get(k) - 1);
-            } else {
-                a[i] = -1;
+            String s = B[i];
+            int[] z = new int[26];
+            Arrays.fill(z, 0);
+            for (int j = 0; j < s.length(); j++) {
+                z[s.charAt(j) - 'a']++;
             }
+            b.add(z);
         }
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == -1) {
-                int k = map.firstKey();
-                a[i] = k;
-                if (map.get(k) == 1)
-                    map.remove(k);
-                else
-                    map.put(k, map.get(k) - 1);
 
+        for (int j = 0; j < a.size(); j++) {
+
+            if(answer.contains(A[j])) continue;
+            int[] x = a.get(j);
+            boolean v = true;
+            for (int i = 0; i < b.size(); i++) {
+                int[] z = b.get(i);
+
+                boolean is = true;
+
+                for (int k = 0; k < 26; k++) {
+                    if (x[k] < z[k]) {
+                        is = false;
+                        break;
+                    }
+                }
+                if (!is) {
+                    v = false;
+                    break;
+                }
             }
+            if (v)
+                answer.add(A[j]);
         }
-        return a;
+
+        return new ArrayList<>(answer);
     }
 
 }
