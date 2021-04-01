db5b789913605d4c18f7e5c57e9cc22c2e585085
Abhilash Das <abhilashmyworld@gmail.com>
2021-04-01 22:54:34 +0530

    solve "Palindrome Linked List"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,72 +1,25 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import app.zollet.leetcode.dsalgo.util.ListNode;
 
 public class LeetCodeSolution {
 
 
     public void execute() {
-        List<String> a = wordSubsets(new String[]{"amazon", "apple", "facebook", "google", "leetcode"}, new String[]{"e", "o"});
+
     }
 
-    public List<String> wordSubsets(String[] A, String[] B) {
-        List<int[]> a = new ArrayList<>();
-        Set<String> answer = new HashSet<>();
-        for (int i = 0; i < A.length; i++) {
-            String s = A[i];
+    public boolean isPalindrome(ListNode head) {
 
-            int[] z = new int[26];
-            Arrays.fill(z, 0);
-            for (int j = 0; j < s.length(); j++) {
-                z[s.charAt(j) - 'a']++;
-            }
-            a.add(z);
+        StringBuilder s = new StringBuilder();
+
+        while (head != null) {
+            s.append(head.val);
+            head = head.next;
         }
 
-        List<int[]> b = new ArrayList<>();
-
-        for (int i = 0; i < B.length; i++) {
-            String s = B[i];
-            int[] z = new int[26];
-            Arrays.fill(z, 0);
-            for (int j = 0; j < s.length(); j++) {
-                z[s.charAt(j) - 'a']++;
-            }
-            b.add(z);
-        }
-
-
-        for (int j = 0; j < a.size(); j++) {
-
-            if(answer.contains(A[j])) continue;
-            int[] x = a.get(j);
-            boolean v = true;
-            for (int i = 0; i < b.size(); i++) {
-                int[] z = b.get(i);
-
-                boolean is = true;
-
-                for (int k = 0; k < 26; k++) {
-                    if (x[k] < z[k]) {
-                        is = false;
-                        break;
-                    }
-                }
-                if (!is) {
-                    v = false;
-                    break;
-                }
-            }
-            if (v)
-                answer.add(A[j]);
-        }
-
-        return new ArrayList<>(answer);
+        return s.toString().equals(s.reverse().toString());
     }
 
 }
