2632f9da63e7ff1a2f398702f6e2205fa3e638d0
Abhilash Das <abhilashmyworld@gmail.com>
2021-05-01 19:44:21 +0530

    solve "Climbing Stairs"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,7 +1,8 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import app.zollet.leetcode.dsalgo.util.ListNode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LeetCodeSolution {
 
@@ -10,16 +11,26 @@ public class LeetCodeSolution {
 
     }
 
-    public boolean isPalindrome(ListNode head) {
 
-        StringBuilder s = new StringBuilder();
+//    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+//
+//    }
 
-        while (head != null) {
-            s.append(head.val);
-            head = head.next;
-        }
+    public int climbStairs(int n) {
 
-        return s.toString().equals(s.reverse().toString());
+        return solve(n,new HashMap<>());
+
+    }
+
+    private int solve(int n, Map<Integer, Integer> map) {
+
+        if (map.containsKey(n)) return map.get(n);
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+
+        int v = climbStairs(n - 1) + climbStairs(n - 2);
+        map.put(n, v);
+        return v;
     }
 
 }
