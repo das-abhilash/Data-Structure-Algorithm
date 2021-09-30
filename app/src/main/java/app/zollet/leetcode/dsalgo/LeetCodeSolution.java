efc565020977dfafb282cb4edf4d238c27953eec
Abhilash Das <abhilashmyworld@gmail.com>
2021-09-30 13:33:35 +0530

    solve "stone game"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,51 +1,30 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-
-import static app.zollet.leetcode.dsalgo.util.TreeNodeUtill.insertLevelOrder;
-
 public class LeetCodeSolution {
 
 
     public void execute() {
 
-
-        TreeNode root = insertLevelOrder(new int[]{5, 4, 8, 11, -1, 13, 4, 7, 2, -1, -1, 5, 1}, new TreeNode(5), 0);
-        pathSum(root,22);
     }
 
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    public boolean stoneGame(int[] piles) {
 
-        List<List<Integer>> answer = new ArrayList<>();
 
-        List<Integer> list = new ArrayList<>();
-        solve(root, targetSum, answer, list);
-        return answer;
+        return solve(piles, true, 0, 0, 0, piles.length-1);
+
     }
 
-    private void solve(TreeNode node, int targetSum, List<List<Integer>> answer, List<Integer> list) {
+    private boolean solve(int[] piles, boolean isAlice, int alice, int bob, int start, int end) {
 
-        if(node == null) return;
-        if (node.left == null && node.right == null) {
-            if (targetSum - node.val== 0) {
-                list.add(node.val);
-                answer.add(new ArrayList<>(list));
-                list.remove(list.size() - 1);
-            }
-            return;
+        if (start > end || start >= piles.length || end < 0) {
+            return alice > bob;
         }
-        list.add(node.val);
-        if (node.left != null) {
-            solve(node.left, targetSum - node.val, answer, list);
+
+        if (isAlice) {
+            return solve(piles, !isAlice, alice + piles[start], bob, start + 1, end) || solve(piles, !isAlice, alice + piles[end], bob, start, end - 1);
+        } else {
+            return solve(piles, !isAlice, alice, bob + piles[start], start + 1, end) || solve(piles, !isAlice, alice, bob + piles[end], start, end - 1);
         }
-        if (node.right != null) {
-            solve(node.right, targetSum - node.val, answer, list);
-        }
-        list.remove(list.size() - 1);
     }
-
 }
