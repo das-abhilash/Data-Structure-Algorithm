a553aadf7c5f41d1d69f62e8eeae2b67f2d41fd3
Abhilash Das <abhilashmyworld@gmail.com>
2021-10-05 14:46:08 +0530

    solve "Climbing Stairs"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -9,45 +9,25 @@ public class LeetCodeSolution {
 
     public void execute() {
 
-
-        int s = minCut("aab");
     }
 
-    public int minCut(String s) {
-        Map<String, Integer> dp = new HashMap<>();
-        Map<String, Boolean> pall = new HashMap<>();
-        int sf = solve(s, 0, s.length() - 1, dp, pall);
+    public int climbStairs(int n) {
 
-        return sf;
-    }
 
-    private int solve(String s, int start, int end, Map<String, Integer> dp, Map<String, Boolean> pall) {
-        if (start == end || isPalindrome(s, start, end, pall)) return 0;
-
-        String key = start + "_" + end;
-        if (dp.containsKey(key)) return dp.get(key);
-
-        int min = s.length() - 1;
-
-        for (int i = start; i <= end; i++) {
-            if (isPalindrome(s, start, i, pall))
-                min = Math.min(min, 1 + solve(s, i + 1, end, dp, pall));
-        }
-
-        dp.put(key, min);
-        return min;
+        Map<Integer, Integer> dp = new HashMap<>();
+        return solve(n, dp);
 
     }
 
+    private int solve(int n, Map<Integer, Integer> dp) {
+        if (n < 0) return 0;
+        if (n < 2) return n;
+        if (dp.containsKey(n)) return dp.get(n);
 
-    private boolean isPalindrome(String value, int start, int end, Map<String, Boolean> pall) {
-        if (start >= end) return true;
-        String key = start + "_" + end;
-        if (pall.containsKey(key)) return pall.get(key);
-        String pal = value.substring(start, end + 1);
-        String reverse = (new StringBuilder(pal)).reverse().toString();
-        boolean iss = pal.equals(reverse);
-        pall.put(key, iss);
-        return iss;
+
+        int ways = solve(n - 1, dp) + solve(n - 2, dp);
+        dp.put(n, ways);
+
+        return ways;
     }
 }
