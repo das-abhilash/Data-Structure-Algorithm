3edbf2b7f7b2a224cd19a227a3fb379cb4b52b1c
Abhilash Das <abhilashmyworld@gmail.com>
2021-10-06 14:08:22 +0530

    solve "Find All Duplicates in an Array"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,33 +1,28 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeetCodeSolution {
 
 
     public void execute() {
 
+        findDuplicates(new int[]{4,3,2,7,8,2,3,1});
     }
 
-    public int climbStairs(int n) {
+    public List<Integer> findDuplicates(int[] nums) {
 
 
-        Map<Integer, Integer> dp = new HashMap<>();
-        return solve(n, dp);
+        List<Integer> answer = new ArrayList<>();
 
-    }
-
-    private int solve(int n, Map<Integer, Integer> dp) {
-        if (n < 0) return 0;
-        if (n < 2) return n;
-        if (dp.containsKey(n)) return dp.get(n);
-
-
-        int ways = solve(n - 1, dp) + solve(n - 2, dp);
-        dp.put(n, ways);
-
-        return ways;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) -1] < 0) {
+                answer.add(Math.abs(nums[i]));
+            }
+            nums[Math.abs(nums[i])-1] = nums[Math.abs(nums[i])-1] * -1;
+        }
+        return answer;
     }
 }
