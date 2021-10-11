925df7735287a1f60d61a001d5f05184b62636cc
Abhilash Das <abhilashmyworld@gmail.com>
2021-10-11 17:15:14 +0530

    solve "Diameter of Binary Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,51 +1,33 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import app.zollet.leetcode.dsalgo.util.TreeNode;
 
 public class LeetCodeSolution {
 
 
+
+
     public void execute() {
+
+    }
+    int d = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        d = 0;
+
+        solve(root);
+        return d;
     }
 
-    public List<String> findWords(char[][] board, String[] words) {
+    private int solve(TreeNode root) {
+        if (root == null) return 0;
 
-        List<String> answer = new ArrayList<>();
-        for (int k = 0; k < words.length; k++) {
-            boolean found = false;
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (board[i][j] == words[k].charAt(0)) {
-                        if (solve(board, words[k], 0, i, j)) {
-                            found = true;
-                            break;
-                        }
-                    }
-                }
-                if (found) break;
-            }
-            if (found) answer.add(words[k]);
-        }
-        return answer;
+        int left = solve(root.left);
+        int right = solve(root.right);
+
+        d = Math.max(left + right + 1, d);
+
+        return Math.max(left, right) + 1;
     }
-
-    private boolean solve(char[][] board, String word, int index, int i, int j) {
-        if (index >= word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '$')
-            return false;
-        if (word.charAt(index) != board[i][j]) return false;
-        char c = board[i][j];
-        board[i][j] = '$';
-        boolean a = solve(board, word, index + 1, i + 1, j)
-                || solve(board, word, index + 1, i - 1, j)
-                || solve(board, word, index + 1, i, j + 1)
-                || solve(board, word, index + 1, i, j - 1);
-
-        board[i][j] = c;
-        return a;
-    }
-
 
 }
