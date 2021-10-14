a234616eb59cea010808d1b212c5270caadb6f48
Abhilash Das <abhilashmyworld@gmail.com>
2021-10-14 20:54:31 +0530

    solve "Guess Number Higher or Lower"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,33 +1,34 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import app.zollet.leetcode.dsalgo.util.TreeNode;
-
 public class LeetCodeSolution {
-
-
 
 
     public void execute() {
 
     }
-    int d = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        d = 0;
 
-        solve(root);
-        return d;
+    public int guessNumber(int n) {
+
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            int mid = (r - l) / 2;
+
+            if (guess(mid) == 0) return mid;
+
+            if (guess(mid) == -1) {
+                r = mid - 1;
+            }
+            if (guess(mid) == 1) {
+                l = mid + 1;
+            }
+        }
+        return -1;
     }
 
-    private int solve(TreeNode root) {
-        if (root == null) return 0;
-
-        int left = solve(root.left);
-        int right = solve(root.right);
-
-        d = Math.max(left + right + 1, d);
-
-        return Math.max(left, right) + 1;
+    int guess(int num) {
+        return 1;
     }
 
 }
