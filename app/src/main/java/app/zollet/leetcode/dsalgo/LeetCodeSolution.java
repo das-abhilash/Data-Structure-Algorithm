3486e0f25dd5a3c027594895697b15e713cd617b
Abhilash Das <abhilashmyworld@gmail.com>
2021-10-15 17:07:10 +0530

    solve "Construct Binary Search Tree from Preorder Traversal"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,28 +1,42 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+
 public class LeetCodeSolution {
+
 
 
     public void execute() {
 
-        int s = maxProfit(new int[]{1, 2, 4});
+        TreeNode df = bstFromPreorder(new int[]{8,5,1,7,10,12});
+
+    }
+    int i = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder.length == 0) return null;
+        i = 0;
+        TreeNode root = new TreeNode(preorder[0]);
+        i++;
+        solve(root, preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return root;
     }
 
-    public int maxProfit(int[] prices) {
-
-        int buy = -prices[0];
-        int sell = 0;
-        int prevBuy = 0;
-        int prevSell = 0;
-
-        for (int i = 0; i < prices.length; i++) {
-            prevBuy = buy;
-            buy = Math.max(prevSell - prices[i], buy);
-            prevSell = sell;
-            sell = Math.max(prevBuy + prices[i], sell);
+    private void solve(TreeNode root, int[] preorder, int min, int max) {
+        if(i>= preorder.length) return;
+        if (preorder[i] > min && preorder[i] < root.val) {
+            TreeNode left = new TreeNode(preorder[i]);
+            root.left = left;
+            i++;
+            solve(root.left, preorder, min, root.val);
         }
-        return sell;
+        if(i>= preorder.length) return;
+        if (preorder[i] < max && preorder[i] > root.val) {
+            TreeNode right = new TreeNode(preorder[i]);
+            root.right = right;
+            i++;
+            solve(root.right, preorder, root.val, max);
+        }
     }
 
 }
