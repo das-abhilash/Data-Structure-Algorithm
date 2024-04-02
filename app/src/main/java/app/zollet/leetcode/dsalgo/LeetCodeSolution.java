45e4732216d3e62057ca1fcb1224a44ae7fc354a
abhilash das <abhilash.das@cred.club>
2024-04-03 00:51:37 +0530

    solve: "Isomorphic Strings"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,23 +1,38 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.Arrays;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
     }
 
-    public int lengthOfLastWord(String s) {
-        String input = s.trim();
-        int answer = 0;
-        for (int i = input.length() -1; i >=0; i--) {
-            if(input.charAt(i)  == ' ') {
-                break;
-            } else {
-                answer++;
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.equals(t)) return true;
+        int[] charMap = new int[256];
+        int[] countMap = new int[256];
+        int[] countMap2 = new int[256];
+
+        Arrays.fill(charMap, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (charMap[(int) s.charAt(i)] != -1 && charMap[(int) s.charAt(i)] != (int) t.charAt(i)) {
+                return false;
             }
+
+
+            charMap[(int) s.charAt(i)] = (int) t.charAt(i);
+            countMap[(int) s.charAt(i)] = countMap[(int) s.charAt(i)] + 1;
+            countMap2[(int) t.charAt(i)] = countMap2[(int) t.charAt(i)] + 1;
+
+            if (countMap[(int) s.charAt(i)] != countMap2[(int) t.charAt(i)]) return false;
         }
-        return answer;
+
+        return true;
     }
 
 }
