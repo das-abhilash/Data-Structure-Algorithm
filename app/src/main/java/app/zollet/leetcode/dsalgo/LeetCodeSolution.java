55bade5b24c03003fd20902f59323037e3885019
abhilash das <abhilash.das@cred.club>
2024-04-04 00:13:09 +0530

    solve: "Word Search"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -9,30 +9,43 @@ public class LeetCodeSolution {
 
     }
 
-    public boolean isIsomorphic(String s, String t) {
+    public boolean exist(char[][] board, String word) {
+        if (word.isEmpty()) return false;
+        char start = word.charAt(0);
 
-        if (s.equals(t)) return true;
-        int[] charMap = new int[256];
-        int[] countMap = new int[256];
-        int[] countMap2 = new int[256];
+        boolean[][] dp = new boolean[board.length][board[0].length];
 
-        Arrays.fill(charMap, -1);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == start) {
+                    if (check(board, dp, word, 1, i, j)) return true;
+                }
 
-        for (int i = 0; i < s.length(); i++) {
-
-            if (charMap[(int) s.charAt(i)] != -1 && charMap[(int) s.charAt(i)] != (int) t.charAt(i)) {
-                return false;
             }
-
-
-            charMap[(int) s.charAt(i)] = (int) t.charAt(i);
-            countMap[(int) s.charAt(i)] = countMap[(int) s.charAt(i)] + 1;
-            countMap2[(int) t.charAt(i)] = countMap2[(int) t.charAt(i)] + 1;
-
-            if (countMap[(int) s.charAt(i)] != countMap2[(int) t.charAt(i)]) return false;
         }
-
-        return true;
+        return false;
     }
+
+    private boolean check(char[][] board, boolean[][] dp, String word, int index, int x, int y) {
+        if (index >= word.length()) return true;
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
+
+
+
+        if (dp[x][y]) return false;
+
+
+        if (board[x][y] != word.charAt(index)) return false;
+
+        dp[x][y] = true;
+        if (check(board, dp, word, index + 1, x + 1, y)) return true;
+        if (check(board, dp, word, index + 1, x - 1, y)) return true;
+        if (check(board, dp, word, index + 1, x, y + 1)) return true;
+        if( check(board, dp, word, index + 1, x, y - 1)) return true;
+
+        dp[x][y] = false;
+        return false;
+    }
+
 
 }
