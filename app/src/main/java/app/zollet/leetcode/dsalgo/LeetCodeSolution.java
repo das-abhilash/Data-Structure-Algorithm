f478046077fe72bbf7ba73324ee339597fd84c11
abhilash das <abhilash.das@cred.club>
2024-04-04 12:00:30 +0530

    solve: "Maximum Nesting Depth of the Parentheses"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,51 +1,25 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
 
     public void execute() {
 
     }
 
-    public boolean exist(char[][] board, String word) {
-        if (word.isEmpty()) return false;
-        char start = word.charAt(0);
+    public int maxDepth(String s) {
+        int ans = 0;
+        int curDepth = 0;
+        for (int i = 0; i < s.length(); i++) {
 
-        boolean[][] dp = new boolean[board.length][board[0].length];
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == start) {
-                    if (check(board, dp, word, 1, i, j)) return true;
-                }
-
+            if (s.charAt(i) == '(') {
+                curDepth++;
+                ans = Math.max(curDepth, ans);
+            } else if (s.charAt(i) == ')') {
+                curDepth--;
             }
         }
-        return false;
+        return ans;
+
     }
-
-    private boolean check(char[][] board, boolean[][] dp, String word, int index, int x, int y) {
-        if (index >= word.length()) return true;
-        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
-
-
-
-        if (dp[x][y]) return false;
-
-
-        if (board[x][y] != word.charAt(index)) return false;
-
-        dp[x][y] = true;
-        if (check(board, dp, word, index + 1, x + 1, y)) return true;
-        if (check(board, dp, word, index + 1, x - 1, y)) return true;
-        if (check(board, dp, word, index + 1, x, y + 1)) return true;
-        if( check(board, dp, word, index + 1, x, y - 1)) return true;
-
-        dp[x][y] = false;
-        return false;
-    }
-
-
 }
