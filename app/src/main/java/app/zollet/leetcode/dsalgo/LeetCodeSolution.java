de9a3268d52690ca1c110036ed8b41950ea494bd
abhilash das <abhilash.das@cred.club>
2024-04-05 15:49:47 +0530

    solve: "Make The String Great"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,25 +1,35 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.Stack;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
+        String out = makeGood("leEeetcode");
     }
 
-    public int maxDepth(String s) {
-        int ans = 0;
-        int curDepth = 0;
-        for (int i = 0; i < s.length(); i++) {
+    //abBAcC
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<Character>();
 
-            if (s.charAt(i) == '(') {
-                curDepth++;
-                ans = Math.max(curDepth, ans);
-            } else if (s.charAt(i) == ')') {
-                curDepth--;
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+            } else if (stack.peek() == s.charAt(i)) {
+                stack.push(s.charAt(i));
+            } else if (Character.toLowerCase(stack.peek()) == Character.toLowerCase(s.charAt(i))) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
             }
         }
-        return ans;
 
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
     }
 }
--- app/src/main/java/app/zollet/leetcode/dsalgo/MainActivity.kt
+++ app/src/main/java/app/zollet/leetcode/dsalgo/MainActivity.kt
@@ -3,9 +3,9 @@ package app.zollet.leetcode.dsalgo
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.airbnb.lottie.LottieDrawable
-import io.reactivex.Single
-import kotlinx.android.synthetic.main.activity_main.*
+//import com.airbnb.lottie.LottieDrawable
+//import io.reactivex.Single
+//import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -23,18 +23,18 @@ class MainActivity : AppCompatActivity() {
 //        val solution = CodeForcesSolution()
 //        solution.execute()
         val anim = "${Random.nextInt(6)}.json"
-        animation.setAnimation(anim)
+/*        animation.setAnimation(anim)
         animation.repeatCount = LottieDrawable.INFINITE
         animation.playAnimation();
         val g = Single.create<Int> {
             it.onSuccess(5)
             it.onSuccess(4)
             it.onSuccess(3)
-        }
-            .blockingGet()
+        }*/
+//            .blockingGet()
 //        val f = sdf()
 //        f.score = 10
-        Log.d("meg", "${g} :result")
+//        Log.d("meg", "${g} :result")
     }
 
     class sdf {
