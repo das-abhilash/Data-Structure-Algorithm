a7aad9c00848f2bdce3810ad09b937cfa016f06b
abhilash das <abhilash.das@cred.club>
2024-04-07 08:08:11 +0530

    solve: "valid parenthesis string"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -6,29 +6,50 @@ import java.util.Stack;
 public class LeetCodeSolution {
 
     public void execute() {
-
-        String out = makeGood("leEeetcode");
+        minRemoveToMakeValid("(a(b(c)d)");
+// (a(b(c)d)
     }
 
-    //abBAcC
-    public String makeGood(String s) {
-        Stack<Character> stack = new Stack<Character>();
+    // a -> ( a ( b  c ) d )
+    // b ->
+    public String minRemoveToMakeValid(String s) {
+
+        Stack<Character> actual = new Stack<>();
+        Stack<Character> bracket = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (stack.isEmpty()) {
-                stack.push(s.charAt(i));
-            } else if (stack.peek() == s.charAt(i)) {
-                stack.push(s.charAt(i));
-            } else if (Character.toLowerCase(stack.peek()) == Character.toLowerCase(s.charAt(i))) {
-                stack.pop();
+            char c = s.charAt(i);
+            if (c == '(') {
+
+                bracket.push(c);
+                actual.push(c);
+
+            } else if (c == ')') {
+
+                if (!bracket.isEmpty() && bracket.peek() == '(') {
+                    bracket.pop();
+                    actual.push(c);
+                }
+
             } else {
-                stack.push(s.charAt(i));
+                actual.push(c);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.insert(0, stack.pop());
+
+        while (!actual.isEmpty()) {
+
+            if (actual.peek() == ')' || actual.peek() != '(') {
+                sb.insert(0, actual.pop());
+            } else {
+                if (bracket.isEmpty()) {
+                    sb.insert(0, actual.pop());
+                } else {
+                    actual.pop();
+                    bracket.pop();
+                }
+            }
         }
         return sb.toString();
     }
