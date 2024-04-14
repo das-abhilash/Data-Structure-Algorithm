322c627dea433f9ba0b48917c69b23b0d7677207
abhilash das <abhilash.das@cred.club>
2024-04-14 08:32:22 +0530

    solve: "Latest Time You Can Obtain After Replacing Characters"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,20 +1,45 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class LeetCodeSolution {
 
     public void execute() {
+        String df = findLatestTime("1?:?4");
     }
 
-    public int scoreOfString(String s) {
-        int ans = 0;
+    public String findLatestTime(String s) {
+        return fixHour(s.charAt(0), s.charAt(1)) + ":" + fixMin(s.charAt(3), s.charAt(4));
+    }
 
-        for (int i = 1; i < s.length(); i++) {
-            ans = ans + Math.abs(s.charAt(i) - s.charAt(i-1));
+    public String fixHour(char i, char j) {
+
+        if (i == '?' && j == '?') {
+            return "11";
+        } else if (i == '?') {
+            if ( j == 49 || j == 48) {
+                return "1" + j;
+            } else {
+                return "0" + j;
+            }
+        } else if (j == '?') {
+            if ( i == 49 ) {
+                return "11";
+            } else {
+                return "09";
+            }
         }
-        return ans;
+        return String.valueOf(i) + j;
     }
+
+    public String fixMin(char i, char j) {
+        if (i == '?' && j == '?') {
+            return "59";
+        } else if (i == '?') {
+            return "5" + j;
+        } else if (j == '?') {
+            return i + "9";
+        }
+        return String.valueOf(i) + j;
+    }
+
 }
