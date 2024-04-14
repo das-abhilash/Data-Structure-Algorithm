9e680ed027a6676914a7238417db62484cd017a4
abhilash das <abhilash.das@cred.club>
2024-04-14 08:00:26 +0530

    solve: "score of string"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,40 +1,20 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        int[] students = { 1,1,0,0 };
-        int[] sandwiches = { 0,1,0,1 };
-        countStudents(students,sandwiches);
     }
 
-    public int countStudents(int[] students, int[] sandwiches) {
+    public int scoreOfString(String s) {
+        int ans = 0;
 
-        if (students.length == 0) return 0;
-        int index = 0;
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 0; i < students.length; i++) {
-            queue.add(students[i]);
+        for (int i = 1; i < s.length(); i++) {
+            ans = ans + Math.abs(s.charAt(i) - s.charAt(i-1));
         }
-
-        int notAte = 0;
-        while (!queue.isEmpty()) {
-            int student = queue.poll();
-            if (sandwiches[index] == student) {
-                index++;
-                notAte = 0;
-            } else {
-                queue.add(student);
-                notAte++;
-            }
-            if(notAte == students.length) return students.length - index;
-        }
-        return 0;
+        return ans;
     }
 }
