4508b0f958fb1931cb6648cd9ff8083670b899dd
abhilash das <abhilash.das@cred.club>
2024-04-19 19:33:07 +0530

    solve: "Minimum Rectangles to Cover Points"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,38 +1,32 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
-        int a = numIslands(new char[][]{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}});
+        int sdf = minRectanglesToCoverPoints(new int[][]{{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}}, 2);
     }
 
-    public int numIslands(char[][] grid) {
+    public int minRectanglesToCoverPoints(int[][] points, int w) {
 
-        boolean[][] dp = new boolean[grid.length][grid[0].length];
 
-        int ans = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !dp[i][j]) {
-                    ans++;
-                    dfs(grid, dp, i, j);
-                }
+        Arrays.sort(points, Comparator.comparingInt(a -> a[0]));
+
+        int ans = 1;
+        int last = points[0][0];
+        for (int i = 1; i < points.length - 1; i++) {
+            if (points[i][0] - last > w) {
+                last = points[i + 1][0];
+                ans++;
             }
         }
+
         return ans;
-    }
 
-    private void dfs(char[][] grid, boolean[][] dp, int x, int y) {
-        if (x < 0 || y < 0 || x == grid.length || y == grid[0].length || grid[x][y] == '0' || dp[x][y])
-            return;
-
-        dp[x][y] = true;
-        dfs(grid, dp, x + 1, y);
-        dfs(grid, dp, x - 1, y);
-        dfs(grid, dp, x, y + 1);
-        dfs(grid, dp, x, y - 1);
     }
 
 
