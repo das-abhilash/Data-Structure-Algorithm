795612fb5c50e3acff17ed80cc7f18ef4c8156d0
abhilash das <abhilash.das@cred.club>
2024-04-19 10:46:34 +0530

    solve: "Number of Islands"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -5,57 +5,34 @@ public class LeetCodeSolution {
 
     public void execute() {
 
-        int df = islandPerimeter(new int[][]{{1, 1}});
+        int a = numIslands(new char[][]{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}});
     }
 
+    public int numIslands(char[][] grid) {
 
-    public int islandPerimeter(int[][] grid) {
+        boolean[][] dp = new boolean[grid.length][grid[0].length];
 
-        int p = 0;
+        int ans = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    p += getPerimeter(grid, i, j);
+                if (grid[i][j] == '1' && !dp[i][j]) {
+                    ans++;
+                    dfs(grid, dp, i, j);
                 }
             }
         }
-        return p;
+        return ans;
     }
 
+    private void dfs(char[][] grid, boolean[][] dp, int x, int y) {
+        if (x < 0 || y < 0 || x == grid.length || y == grid[0].length || grid[x][y] == '0' || dp[x][y])
+            return;
 
-    private int getPerimeter(int[][] grid, int x, int y) {
-        int perimeter = 0;
-        if (x - 1 < 0) {
-            perimeter++;
-        } else {
-            if (grid[x - 1][y] == 0) {
-                perimeter++;
-            }
-        }
-        if (y - 1 < 0) {
-            perimeter++;
-        } else {
-            if (grid[x][y - 1] == 0) {
-                perimeter++;
-            }
-        }
-        if (x + 1 >= grid.length) {
-            perimeter++;
-        } else {
-            if (grid[x + 1][y] == 0) {
-                perimeter++;
-            }
-        }
-        if (y + 1 >= grid[0].length) {
-            perimeter++;
-        } else {
-            if (grid[x][y + 1] == 0) {
-                perimeter++;
-            }
-        }
-
-
-        return perimeter;
+        dp[x][y] = true;
+        dfs(grid, dp, x + 1, y);
+        dfs(grid, dp, x - 1, y);
+        dfs(grid, dp, x, y + 1);
+        dfs(grid, dp, x, y - 1);
     }
 
 
