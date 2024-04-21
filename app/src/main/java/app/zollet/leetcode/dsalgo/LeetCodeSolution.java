e8eab26c1e09d9a9dbd2ed41c151e2f25434e8cf
abhilash das <abhilash.das@cred.club>
2024-04-21 08:34:57 +0530

    solve: "Count the Number of Special Characters II"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,29 +1,34 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.Arrays;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
+        int a = numberOfSpecialChars("AbBCab");
     }
 
     public int numberOfSpecialChars(String word) {
 
-        boolean[] u = new boolean[26];
-        boolean[] l = new boolean[26];
-
+        int[] l = new int[26];
+        int[] u = new int[26];
+        Arrays.fill(u,-1);
+        Arrays.fill(l,-1);
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (Character.isUpperCase(c)) {
-                u[c - 'A'] = true;
+            if (Character.isLowerCase(c)) {
+                l[c - 'a'] = Math.max(l[c - 'a'], i);
             } else {
-                l[c - 'a'] = true;
+                if (u[c - 'A'] == -1)
+                    u[c - 'A'] =  i;
             }
         }
 
         int ans = 0;
         for (int i = 0; i < 26; i++) {
-            if (u[i] && l[i]) {
+            if (l[i] != -1 && u[i] > l[i]) {
                 ans++;
             }
         }
