20a5d0185e105b28e54ffc83b21784f7a7a1f4d7
abhilash das <abhilash.das@cred.club>
2024-04-24 08:44:05 +0530

    solve: "N-th Tribonacci Number"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,67 +1,37 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        List<Integer> a = findMinHeightTrees(1, new int[][]{{1, 0}, {1, 2}, {1, 3}});
+        tribonacci(4);
     }
 
-    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
 
-        if(n == 1) return new ArrayList<>(Arrays.asList(0));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
-        Map<Integer, Set<Integer>> edgeMap = new HashMap<>();
+        int df=  solve(n, dp);
+        return df;
+    }
 
-        for (int i = 0; i < edges.length; i++) {
-            Set<Integer> list1 = edgeMap.getOrDefault(edges[i][0], new HashSet<>());
-            list1.add(edges[i][1]);
-            edgeMap.put(edges[i][0], list1);
+    private int solve(int n, int[] dp) {
+        if (n < 0) return 0;
 
-            Set<Integer> list2 = edgeMap.getOrDefault(edges[i][1], new HashSet<>());
-            list2.add(edges[i][0]);
-            edgeMap.put(edges[i][1], list2);
-        }
+        if (dp[n] >= 0) return dp[n];
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int key : edgeMap.keySet()) {
-            if (edgeMap.get(key).size() == 1) {
-                queue.add(key);
-            }
-        }
-
-        while (n > 2) {
-
-            int size = queue.size();
-            n = n - size;
-
-            for (int i = 0; i < size; i++) {
-                int node = queue.poll();
-
-                int neighbour = edgeMap.get(node).iterator().next();
-
-                edgeMap.get(neighbour).remove((Integer) node);
-
-                if (edgeMap.get(neighbour).size() == 1) {
-                    queue.add(neighbour);
-                }
-            }
-        }
-
-
-        return new ArrayList<>(queue);
-
+        int d= solve(n - 1, dp) + solve(n - 2, dp) + solve(n - 3, dp);
+        dp[n] = d;
+        return d;
     }
 
 
