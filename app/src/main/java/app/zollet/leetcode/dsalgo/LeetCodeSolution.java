fb5d14da0e90b930643bc177823a2906bd0b3abc
abhilash das <abhilash.das@cred.club>
2024-04-27 21:15:21 +0530

    attempt: "Right Triangles"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,104 +1,46 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public boolean canMakeSquare(char[][] grid) {
+    public long numberOfRightTriangles(int[][] grid) {
+
+        Map<Integer, Integer> r = new HashMap<>();
+        Map<Integer, Integer> c = new HashMap<>();
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
 
-                if (leftTop(grid, i, j)) return true;
-                if (leftBottom(grid, i, j)) return true;
-                if (rightTop(grid, i, j)) return true;
-                if (rightBottom(grid, i, j)) return true;
+                if (grid[i][j] == 1) {
+                    r.put(i, r.getOrDefault(i, 0) + 1);
+                    c.put(j, c.getOrDefault(j, 0) + 1);
+                }
             }
         }
-        return false;
-    }
-    private boolean leftTop(char[][] grid, int i, int j) {
 
-        int w = 0;
-        int b = 0;
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                int rr = r.getOrDefault(i, 0);
+                int cc = c.getOrDefault(j, 0);
 
-        if (grid[i][j] == 'B') b++;
-        else w++;
-
-        if (i - 1 >= 0 && j-1 >= 0)
-            if (grid[i - 1][j - 1] == 'B') b++;
-            else w++;
-        if (j - 1 >= 0)
-            if (grid[i][j - 1] == 'B') b++;
-            else w++;
-        if (i - 1 >= 0)
-            if (grid[i - 1][j] == 'B') b++;
-            else w++;
-
-        return w >= 3 || b >= 3;
-
-    }
-
-    private boolean leftBottom(char[][] grid, int i, int j) {
-        int w = 0;
-        int b = 0;
-        if (grid[i][j] == 'B') b++;
-        else w++;
-        if (j - 1 >= 0)
-            if (grid[i][j - 1] == 'B') b++;
-            else w++;
-        if (i + 1 < 3)
-            if (grid[i + 1][j] == 'B') b++;
-            else w++;
-        if (i + 1 < 3 && j - 1 >= 0)
-            if (grid[i + 1][j - 1] == 'B') b++;
-            else w++;
-
-        return w >= 3 || b >= 3;
+                if(grid[i][j] == 0) continue;
+                if (rr < 2) continue;
+                if (cc < 2) continue;
 
 
-    }
+                ans = ans + (rr - 1) * (cc - 1);
 
-    private boolean rightTop(char[][] grid, int i, int j) {
+            }
+        }
+        return ans;
 
-        int w = 0;
-        int b = 0;
-        if (grid[i][j] == 'B') b++;
-        else w++;
-        if (i - 1 >= 0)
-            if (grid[i - 1][j] == 'B') b++;
-            else w++;
-        if (j + 1 < 3)
-            if (grid[i][j + 1] == 'B') b++;
-            else w++;
-        if (i - 1 >= 0 && j + 1 < 3)
-            if (grid[i - 1][j + 1] == 'B') b++;
-            else w++;
-
-        return w >= 3 || b >= 3;
-
-
-    }
-
-    private boolean rightBottom(char[][] grid, int i, int j) {
-
-        int w = 0;
-        int b = 0;
-        if (grid[i][j] == 'B') b++;
-        else w++;
-        if (i + 1 < 3)
-            if (grid[i + 1][j] == 'B') b++;
-            else w++;
-        if (j + 1 < 3)
-            if (grid[i][j + 1] == 'B') b++;
-            else w++;
-        if (i + 1 < 3 && j + 1 < 3)
-            if (grid[i + 1][j + 1] == 'B') b++;
-            else w++;
-
-        return w >= 3 || b >= 3;
     }
 
 
