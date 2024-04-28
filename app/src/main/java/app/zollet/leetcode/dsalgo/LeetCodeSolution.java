bb32606bd9a5912dd1697859c70b1aae8e5feb6d
abhilash das <abhilash.das@cred.club>
2024-04-28 15:55:40 +0530

    solve: "Longest Ideal Subsequence"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -6,41 +6,35 @@ import java.util.Arrays;
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = minimumAddedInteger(new int[]{6, 3, 6, 7}, new int[]{6, 2});
-//        int a = minimumAddedInteger(new int[]{4, 6, 3, 1, 4, 2, 10, 9, 5}, new int[]{5, 10, 3, 2, 6, 1, 9});
-//        int a = minimumAddedInteger(new int[]{4,7,5}, new int[]{5});
-//        int a = minimumAddedInteger(new int[]{4,20,16,12,8}, new int[]{14,18,10});
-//        int a = minimumAddedInteger(new int[]{3,5,5,3}, new int[]{7,7});
+        int a = longestIdealString("acfgbd", 2);
     }
 
-    public int minimumAddedInteger(int[] nums1, int[] nums2) {
-
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < nums1.length; i++) {
-            int diff = nums2[0] - nums1[i];
-            if (check(nums1, nums2, diff)) {
-                ans = Math.min(diff, ans);
-            }
-
+    public int longestIdealString(String s, int k) {
+        int[][] dp = new int[s.length()][27];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return ans;
+        return solve(0, '{', s, k, dp);
     }
 
-    private boolean check(int[] nums1, int[] nums2, int diff) {
 
-        int count = 0;
-        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; i++) {
-            if (nums2[j] - nums1[i] == diff) {
-                j++;
-            } else {
-                count++;
-            }
+    private int solve(int i, char last, String s, int k, int[][] dp) {
+        if (i == s.length()) return 0;
+        if (dp[i][last - 'a'] != -1) return dp[i][last - 'a'];
+
+        int take = 0;
+        int notTake = 0;
+
+        if (last == '{' || Math.abs(s.charAt(i) - last) <= k) {
+            take = 1 + solve(i + 1, s.charAt(i), s, k, dp);
         }
-        return count <= 2;
+
+        notTake = solve(i + 1, last, s, k, dp);
+
+        dp[i][last - 'a'] = Math.max(take, notTake);
+        return dp[i][last - 'a'];
     }
+
 }
 
 
