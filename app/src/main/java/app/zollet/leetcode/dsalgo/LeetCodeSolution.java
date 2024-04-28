5e3d8425a88809b5039a5650c9c0892ef748247f
abhilash das <abhilash.das@cred.club>
2024-04-28 14:03:44 +0530

    solve: "Find the Integer Added to Array II"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,38 +1,46 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = minimumAddedInteger(new int[]{4,7,5}, new int[]{5});
+        int a = minimumAddedInteger(new int[]{6, 3, 6, 7}, new int[]{6, 2});
+//        int a = minimumAddedInteger(new int[]{4, 6, 3, 1, 4, 2, 10, 9, 5}, new int[]{5, 10, 3, 2, 6, 1, 9});
+//        int a = minimumAddedInteger(new int[]{4,7,5}, new int[]{5});
 //        int a = minimumAddedInteger(new int[]{4,20,16,12,8}, new int[]{14,18,10});
 //        int a = minimumAddedInteger(new int[]{3,5,5,3}, new int[]{7,7});
     }
 
     public int minimumAddedInteger(int[] nums1, int[] nums2) {
 
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                int diff = nums2[j] - nums1[i];
-                map.put(diff, map.getOrDefault(diff, 0) + 1);
-            }
-        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
         int ans = Integer.MAX_VALUE;
-        int max = -1;
-        for (Integer key : map.keySet()) {
-            if(map.get(key)>max){
-                ans = Math.min(ans,key);
-                max = map.get(key);
+        for (int i = 0; i < nums1.length; i++) {
+            int diff = nums2[0] - nums1[i];
+            if (check(nums1, nums2, diff)) {
+                ans = Math.min(diff, ans);
             }
+
         }
         return ans;
     }
 
+    private boolean check(int[] nums1, int[] nums2, int diff) {
 
+        int count = 0;
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; i++) {
+            if (nums2[j] - nums1[i] == diff) {
+                j++;
+            } else {
+                count++;
+            }
+        }
+        return count <= 2;
+    }
 }
+
+
