f72ca51fc23d7857ee9602afb076a2feba439b67
abhilash das <abhilash.das@cred.club>
2024-04-28 08:10:45 +0530

    attempt: "Find the Integer Added to Array I"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,61 +1,20 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        long a = numberOfRightTriangles(new int[][]{{1,1,1},{1,0,1}});
     }
 
-    public long numberOfRightTriangles(int[][] grid) {
+    public int addedInteger(int[] nums1, int[] nums2) {
 
-        Map<Integer, Integer> r = new HashMap<>();
-        Map<Integer, Integer> c = new HashMap<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        return nums2[0] - nums1[0];
 
-                if (grid[i][j] == 1) {
-                    r.put(i, r.getOrDefault(i, 0) + 1);
-                    c.put(j, c.getOrDefault(j, 0) + 1);
-                }
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                int rr = r.getOrDefault(i, 0);
-                int cc = c.getOrDefault(j, 0);
-
-                if(grid[i][j] == 0) continue;
-                if (rr < 2) continue;
-                if (cc < 2) continue;
-
-
-                ans = ans + (cc-1) * nCr(rr,2);
-
-            }
-        }
-        return ans;
-
-    }
-
-
-    private int nCr(int n, int r)
-    {
-        return fact(n) / (fact(r) *
-                fact(n - r));
-    }
-    private int fact(int n)
-    {
-        int res = 1;
-        for (int i = 2; i <= n; i++)
-            res = res * i;
-        return res;
     }
 
 }
