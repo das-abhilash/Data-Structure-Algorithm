739d4d36b79be5b4978d5b8c03ae164662ed1630
abhilash das <abhilash.das@cred.club>
2024-05-02 08:36:21 +0530

    solve: "Lexicographically Smallest String After Operations With Constraint"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -6,24 +6,33 @@ public class LeetCodeSolution {
     public void execute() {
     }
 
-    public String reversePrefix(String word, char ch) {
+    public String getSmallestString(String s, int k) {
+
         StringBuilder sb = new StringBuilder();
 
-        boolean shouldReverse = true;
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (shouldReverse) {
-                sb.append(c);
-                if (c == ch) {
-                    result.append(sb.reverse());
-                    shouldReverse = false;
-                }
+        int index = 0;
+        for (; index < s.length(); index++) {
+            char c = s.charAt(index);
+            int left = c - 'a';
+            int right = 'z' - c + 1;
+
+            int dis = Math.min(left, right);
+
+            if (k >= dis) {
+                sb.append('a');
+                k = k - dis;
             } else {
-                result.append(c);
+                sb.append((char) (c - k));
+                break;
             }
+
         }
-        return shouldReverse ? sb.toString() : result.toString();
+
+        for (int i = index+1; i <s.length() ; i++) {
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
     }
 
 }
