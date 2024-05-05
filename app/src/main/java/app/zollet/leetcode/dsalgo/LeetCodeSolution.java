033673960d4e760acd01266e75160a17d0f71239
abhilash das <abhilash.das@cred.club>
2024-05-06 00:39:47 +0530

    solve: "Minimum Length of Anagram Concatenation"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,22 +1,62 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import app.zollet.leetcode.dsalgo.util.ListNode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    public void deleteNode(ListNode node) {
+    public int minAnagramLength(String s) {
 
-        int next  = node.next.val;
-        ListNode n = node.next.next;
+        List<Integer> divisors = new ArrayList<>();
 
-        node.val = next;
+        int n = s.length();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                int k = n / i;
+                if (k == i) {
+                    divisors.add(k);
+                } else {
+                    divisors.add(k);
+                    divisors.add(i);
+                }
+            }
+        }
 
-        node.next = n;
+        Collections.sort(divisors);
 
+
+        for (int i = 0; i < divisors.size(); i++) {
+            int ans = divisors.get(i);
+            Map<Character, Integer> pre = new HashMap<>();
+            for (int j = 0; j < ans; j++) {
+                pre.put(s.charAt(j), pre.getOrDefault(s.charAt(j), 0) + 1);
+            }
+
+            boolean isMatched = true;
+
+            for (int j = ans; j < n; j = j + ans) {
+                Map<Character, Integer> post = new HashMap<>();
+                for (int k = j; k < j + ans; k++) {
+                    post.put(s.charAt(k), post.getOrDefault(s.charAt(k), 0) + 1);
+                }
+
+                if(!pre.equals(post)){
+                    isMatched = false;
+                    break;
+                }
+            }
+
+            if(isMatched) return ans;
+
+        }
+        return n;
     }
 
 }
