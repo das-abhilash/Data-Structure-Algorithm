8971a5cbc983e858d536af778b92348800145cc4
abhilash das <abhilash.das@cred.club>
2024-05-05 08:31:50 +0530

    solve: "valid word"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,32 +1,45 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class LeetCodeSolution {
 
     public void execute() {
+
+        boolean df = isValid("UuE6");
     }
 
-    public int findMaxK(int[] nums) {
+    public boolean isValid(String word) {
 
-        Set<Integer> set = new HashSet<>();
+        int charcount = 0;
 
-        for (int n : nums) {
-            set.add(n);
-        }
+        boolean vc = false;
+        boolean cc = false;
 
-        int ans = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (num > 0 && set.contains(num * -1)) {
-                ans = Math.max(ans, num);
+        if (word.length() < 3) return false;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '@' || c == '#' || c == '$') {
+                return false;
             }
+
+
+            if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+                continue;
+            }
+
+            charcount++;
+
+            if (c == 'A' || c == 'a' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U') {
+                vc = true;
+                continue;
+            }
+
+            cc = true;
+
         }
 
-        return ans == Integer.MIN_VALUE ? -1 : ans;
+        return vc && cc;
     }
 
 }
