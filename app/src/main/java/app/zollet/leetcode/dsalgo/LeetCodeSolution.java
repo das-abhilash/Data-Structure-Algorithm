23a04c0c81ae073bdcb42e1ffeead7ab1919a40e
abhilash das <abhilash.das@cred.club>
2024-05-05 10:42:48 +0530

    solve: "delete node"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,33 +1,22 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import app.zollet.leetcode.dsalgo.util.ListNode;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        int df= minimumOperationsToMakeKPeriodic("leetcodeleet", 4);
     }
 
-    public int minimumOperationsToMakeKPeriodic(String word, int k) {
+    public void deleteNode(ListNode node) {
 
-        Map<String, Integer> map = new HashMap<>();
+        int next  = node.next.val;
+        ListNode n = node.next.next;
 
-        int max = Integer.MIN_VALUE;
-        for (int i = k; i <= word.length(); i = i + k) {
-            String sub = word.substring( i - k, i);
-            if (map.containsKey(sub)) {
-                int c = map.get(sub) + 1;
-                map.put(sub, c);
-                max = Math.max(max, c);
-            } else {
-                map.put(sub, 1);
-                max = Math.max(max, 1);
-            }
-        }
+        node.val = next;
 
-        return word.length() / k - max;
+        node.next = n;
+
     }
 
 }
