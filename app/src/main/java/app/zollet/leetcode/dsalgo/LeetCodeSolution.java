565cf534e4a0f8600bfae9dcfeab4b033b8dfd60
abhilash das <abhilash.das@cred.club>
2024-05-09 18:20:47 +0530

    solve: "Maximize Happiness of Selected Children"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,41 +1,37 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        String[] hjg = findRelativeRanks(new int[]{5, 4, 3, 2, 1});
+        long sdf = maximumHappinessSum(new int[]{12, 1, 42}, 3);
     }
 
-    public String[] findRelativeRanks(int[] score) {
 
+    public long maximumHappinessSum(int[] happiness, int k) {
 
-        Map<Integer, Integer> map = new HashMap<>();
-        String[] result = new String[score.length];
-        for (int i = 0; i < score.length; i++) {
-            map.put(score[i], i);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int h : happiness) {
+            priorityQueue.add(h);
         }
 
-        Arrays.sort(score);
-
-        for (int i = 0; i < score.length; i++) {
-            int index = map.get(score[i]);
-            result[index] = String.valueOf(score.length - i);
+        long ans = 0;
+        long minus = 0;
+        while (!priorityQueue.isEmpty() && k > 0) {
+            int a = priorityQueue.poll();
+            if (a - minus > 0) {
+                ans = ans + a - minus;
+                minus++;
+            }
+            k--;
         }
-
-        result[map.get(score[score.length - 1])] = "Gold Medal";
-        if (score.length > 1)
-            result[map.get(score[score.length - 2])] = "Silver Medal";
-        if (score.length > 2)
-            result[map.get(score[score.length - 3])] = "Bronze Medal";
-
-        return result;
-
+        return ans;
     }
+
 
 }
 
