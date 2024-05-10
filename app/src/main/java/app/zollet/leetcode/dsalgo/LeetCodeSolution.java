da73a7847fd67ef14bd8c8ee7f623e8ecdb6ccb4
abhilash das <abhilash.das@cred.club>
2024-05-10 19:46:42 +0530

    solve: "K-th Smallest Prime Fraction"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,43 +1,49 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Stack;
-
-import app.zollet.leetcode.dsalgo.util.ListNode;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
+    class Fraction {
+        int nom;
+        int dem;
+        double val;
 
-    public ListNode removeNodes(ListNode head) {
-
-        Stack<Integer> stack = new Stack<>();
-        stack.add(head.val);
-        head = head.next;
-
-        while (head != null) {
-            while (!stack.isEmpty() && stack.peek() < head.val) {
-                stack.pop();
-            }
-
-            stack.add(head.val);
-            head = head.next;
+        Fraction(int nom, int dem, double val) {
+            this.nom = nom;
+            this.dem = dem;
+            this.val = val;
         }
 
-        ListNode dummy = new ListNode();
+    }
 
-        while (!stack.isEmpty()) {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<Fraction> pq = new PriorityQueue<>(Comparator.comparingDouble(o -> o.val));
 
-            ListNode node = new ListNode(stack.pop());
-
-            ListNode temp = dummy.next;
-
-            dummy.next = node;
-            node.next = temp;
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(new Fraction(i, arr.length - 1, (double) arr[i] / arr[arr.length - 1]));
         }
-        return dummy.next;
+
+        while (k > 1) {
+
+            Fraction f = pq.poll();
+
+            if (f.dem - 1 > f.nom)
+                pq.add(new Fraction(f.nom, f.dem - 1, (double) arr[f.nom] / arr[f.dem - 1]));
+
+
+            k--;
+        }
+
+        Fraction a = pq.poll();
+
+        return new int[]{arr[a.nom], arr[a.dem]};
+
     }
 
 
