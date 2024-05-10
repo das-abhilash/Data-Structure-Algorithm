df0935ae350b55adb3ddde3b0216aee217f5e153
abhilash das <abhilash.das@cred.club>
2024-05-10 20:34:14 +0530

    solve: "Remove Nodes From Linked List"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,50 +1,56 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import app.zollet.leetcode.dsalgo.util.ListNode;
 
 public class LeetCodeSolution {
 
     public void execute() {
     }
 
-    class Fraction {
-        int nom;
-        int dem;
-        double val;
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode nextTemp = null;
 
-        Fraction(int nom, int dem, double val) {
-            this.nom = nom;
-            this.dem = dem;
-            this.val = val;
+        // Set each node's next pointer to the previous node
+        while (current != null) {
+            nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
         }
 
+        return prev;
     }
 
-    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Fraction> pq = new PriorityQueue<>(Comparator.comparingDouble(o -> o.val));
 
-        for (int i = 0; i < arr.length; i++) {
-            pq.add(new Fraction(i, arr.length - 1, (double) arr[i] / arr[arr.length - 1]));
+    public ListNode removeNodes(ListNode head) {
+
+        head = reverseList(head);
+
+        int max = 0;
+        ListNode current = head;
+        ListNode next = null;
+        ListNode temp = null;
+        while (current != null) {
+
+
+            if (current.val < max) {
+                current = current.next;
+            } else {
+                max = current.val;
+
+                next = current.next;
+                current.next = temp;
+                temp = current;
+                current = next;
+            }
         }
 
-        while (k > 1) {
-
-            Fraction f = pq.poll();
-
-            if (f.dem - 1 > f.nom)
-                pq.add(new Fraction(f.nom, f.dem - 1, (double) arr[f.nom] / arr[f.dem - 1]));
-
-
-            k--;
-        }
-
-        Fraction a = pq.poll();
-
-        return new int[]{arr[a.nom], arr[a.dem]};
-
+        return temp;
     }
+
 
 
 }
