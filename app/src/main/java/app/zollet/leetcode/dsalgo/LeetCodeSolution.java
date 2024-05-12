567c76ba407bab1878a1a93fbb43c4d558c095a8
abhilash das <abhilash.das@cred.club>
2024-05-12 17:15:19 +0530

    solve: "Taking Maximum Energy From the Mystic Dungeon"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,36 +1,21 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = findPermutationDifference("abc","bac");
+
     }
 
-    public int findPermutationDifference(String s, String t) {
+    public int maximumEnergy(int[] energy, int k) {
 
-        int[] ss = new int[26];
-        Arrays.fill(ss, -1);
-        int[] tt = new int[26];
-        Arrays.fill(tt, -1);
+        int[] dp = new int[energy.length];
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            ss[c - 'a'] = i;
-        }
+        int ans = Integer.MIN_VALUE;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = t.charAt(i);
-            tt[c - 'a'] = i;
-        }
-
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (ss[i] > -1) {
-                ans = ans +  Math.abs(ss[i] - tt[i]);
-            }
+        for (int i = energy.length -1; i >= 0; i--) {
+            dp[i] = energy[i] + ((i + k) < energy.length ? dp[i + k] : 0);
+            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
