3ece085310b3863ed19c5a2b8bbe6d0f11428dd5
abhilash das <abhilash.das@cred.club>
2024-05-12 16:59:44 +0530

    solve: "Permutation Difference between Two Strings"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,38 +1,39 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.Arrays;
+
 public class LeetCodeSolution {
 
     public void execute() {
-        int[][] a = largestLocal(new int[][] {{9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}});
+        int a = findPermutationDifference("abc","bac");
     }
 
-    public int[][] largestLocal(int[][] grid) {
+    public int findPermutationDifference(String s, String t) {
 
-        int[][] ans = new int[grid.length - 2][grid.length - 2];
+        int[] ss = new int[26];
+        Arrays.fill(ss, -1);
+        int[] tt = new int[26];
+        Arrays.fill(tt, -1);
 
-
-        for (int i = 0; i < grid.length - 2; i++) {
-            for (int j = 0; j < grid.length - 2; j++) {
-                ans[i][j] = calculate(grid, i, j);
-            }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ss[c - 'a'] = i;
         }
 
+        for (int i = 0; i < s.length(); i++) {
+            char c = t.charAt(i);
+            tt[c - 'a'] = i;
+        }
 
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (ss[i] > -1) {
+                ans = ans +  Math.abs(ss[i] - tt[i]);
+            }
+        }
         return ans;
     }
-
-    private int calculate(int[][] grid, int x, int y) {
-        int max = 0;
-
-        for (int i = x; i < x + 3; i++) {
-            for (int j = y; j < y + 3; j++) {
-                max = Math.max(max, grid[i][j]);
-            }
-        }
-        return max;
-    }
-
 
 }
 
