a50da4605b9b79ab8f81ec5f808551b19d47213a
abhilash das <abhilash.das@cred.club>
2024-05-17 12:32:33 +0530

    solve: "Evaluate Boolean Binary Tree"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,41 +1,26 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import app.zollet.leetcode.dsalgo.util.TreeNode;
+
 public class LeetCodeSolution {
 
     public void execute() {
 
     }
 
-    public int getMaximumGold(int[][] grid) {
-
-        int max = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] > 0) {
-                    max = Math.max(max, max(grid, new boolean[grid.length][grid[0].length], i, j));
-                }
-            }
-        }
-        return max;
-    }
-
-    private int max(int[][] grid, boolean[][] dp, int x, int y) {
-
-        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || dp[x][y] || grid[x][y] == 0) {
-            return 0;
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
         }
 
-        dp[x][y] = true;
-        int left = max(grid, dp, x - 1, y);
-        int right = max(grid, dp, x + 1, y);
-        int top = max(grid, dp, x, y - 1);
-        int bottom = max(grid, dp, x, y + 1);
-        dp[x][y] = false;
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
 
-        return grid[x][y] + Math.max(Math.max(left, right), Math.max(top, bottom));
+        if (root.val == 2) return left || right;
+        else return left && right;
+
     }
-
 }
 
 
