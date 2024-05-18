8078f9221f20d1246e1c73e751e8e14240e9baee
abhilash das <abhilash.das@cred.club>
2024-05-18 18:11:19 +0530

    solve: "Boats to Save People"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,40 +1,62 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = numRescueBoats(new int[]{3, 2, 2, 1}, 3);
     }
 
-    // 3
-    public int numRescueBoats(int[] people, int limit) {
 
-        Arrays.sort(people);
+    public class Pair {
+        double first;
+        int second;
 
-        int left = 0;
-        int right = people.length - 1;
+        public Pair(double first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
 
-        int boats = 0;
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
 
+        List<Pair> list = new ArrayList<>();
 
-        while (left <= right) {
+        for (int i = 0; i < quality.length; i++) {
+            list.add(new Pair((1.0 * (wage[i]) / quality[i]), quality[i]));
+        }
 
-            if ((people[left] + people[right]) <= limit) {
-                boats++;
-                left++;
-                right--;
-            } else {
-                boats++;
-                right--;
+        Collections.sort(list, Comparator.comparingDouble(o -> o.first));
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        double ans = Double.MAX_VALUE;
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+
+            double ratio = list.get(i).first;
+            int q = list.get(i).second;
+
+            queue.add(q);
+            sum = sum + q;
+
+            if (queue.size() > k) {
+                sum = sum - queue.poll();
             }
+
+            if (queue.size() == k) {
+                ans = Math.min(ans, sum * ratio);
+            }
+
 
         }
 
-
-        return boats;
+        return ans;
     }
 
 }
