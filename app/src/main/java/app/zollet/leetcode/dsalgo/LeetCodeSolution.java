7d326d70d67fc79599483f48f05c4a3d941464ad
abhilash das <abhilash.das@cred.club>
2024-06-04 13:19:47 +0530

    solve: "Reverse String"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -4,44 +4,20 @@ package app.zollet.leetcode.dsalgo;
 public class LeetCodeSolution {
 
     public void execute() {
-        int a = longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth");
     }
 
-    public int longestPalindrome(String s) {
+    public void reverseString(char[] s) {
 
-        int[] lower = new int[26];
-        int[] upper = new int[26];
+        int l = 0;
+        int r = s.length - 1;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isUpperCase(c)) {
-                upper[c - 'A']++;
-            } else {
-                lower[c - 'a']++;
-            }
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
         }
-
-        int ans = 0;
-        boolean odd = false;
-        for (int i = 0; i < 26; i++) {
-            if (lower[i] % 2 == 0) {
-                ans = ans + lower[i];
-            } else {
-                odd = true;
-                ans = ans + lower[i] - 1;
-            }
-
-        }
-        for (int i = 0; i < 26; i++) {
-            if (upper[i] % 2 == 0) {
-                ans = ans + upper[i];
-            } else {
-                odd = true;
-                ans = ans + upper[i] - 1;
-            }
-
-        }
-        return ans + (odd ? 1 : 0);
 
     }
 
