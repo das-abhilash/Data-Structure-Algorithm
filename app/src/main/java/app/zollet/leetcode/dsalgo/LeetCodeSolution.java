84416d72faabfc3c3a7c11f9a8d37d3c288c0f4f
abhilash das <abhilash.das@cred.club>
2024-06-12 09:56:25 +0530

    solve: "Sort Colors"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,51 +1,37 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
-
 public class LeetCodeSolution {
 
 
     public void execute() {
 
-        int[] a = relativeSortArray(new int[]{2,3,1,3,2,4,6,7,9,2,19}, new int[]{2,1,4,3,9,6});
     }
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public void sortColors(int[] nums) {
 
-        int[] map = new int[1000];
-        Arrays.fill(map, 0);
-
-        for (int i = 0; i < arr1.length; i++) {
-            map[arr1[i]] = map[arr1[i]] + 1;
-        }
-
-        int[] ans = new int[arr1.length];
+        int l = 0;
+        int r = nums.length - 1;
 
         int index = 0;
-        for (int i = 0; i < arr2.length; i++) {
-            int count = map[arr2[i]];
-            while (count > 0) {
-                ans[index] = arr2[i];
+
+        while (index <= r) {
+            if (nums[index] == 0) {
+                int temp = nums[l];
+                nums[l] = nums[index];
+                nums[index] = temp;
+                l++;
                 index++;
-                count--;
+            } else if (nums[index] == 1) {
+                index++;
+            } else {
+                int temp = nums[r];
+                nums[r] = nums[index];
+                nums[index] = temp;
+                r--;
             }
-            map[arr2[i]] = 0;
         }
 
-        int fh = 0;
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] == 0) continue;
-            int count = map[i];
-            while (count > 0) {
-                ans[index] = i;
-                index++;
-                count--;
-            }
-            map[i] = 0;
-        }
-
-        return ans;
     }
 
 }
