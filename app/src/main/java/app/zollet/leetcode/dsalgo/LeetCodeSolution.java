0e7c81ac83bc8ba647a9aac4187da138cd602b95
abhilash das <abhilash.das@cred.club>
2024-07-03 22:47:52 +0530

    solve: "All Ancestors of a Node in a Directed Acyclic Graph"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,7 +1,13 @@
 package app.zollet.leetcode.dsalgo;
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class LeetCodeSolution {
 
@@ -10,20 +16,49 @@ public class LeetCodeSolution {
 
     }
 
-    public int minDifference(int[] nums) {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
 
-        Arrays.sort(nums);
+        Map<Integer, List<Integer>> adj = new HashMap<>();
 
-        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            adj.put(i, new ArrayList<>());
+        }
 
-        if (n <= 4) return 0;
-        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < edges.length; i++) {
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
 
-        ans = Math.min(ans, nums[n - 1] - nums[4]);
-        ans = Math.min(ans, nums[n - 2] - nums[3]);
-        ans = Math.min(ans, nums[n - 3] - nums[1]);
-        ans = Math.min(ans, nums[n - 4] - nums[0]);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> l = new ArrayList<>(getAncestors(adj, i, dp));
+            Collections.sort(l);
+            ans.add(l);
+        }
         return ans;
+    }
+
+    private Set<Integer> getAncestors(Map<Integer, List<Integer>> adj, int node, Map<Integer, Set<Integer>> dp) {
+
+        Set<Integer> list = new HashSet<>();
+
+        List<Integer> nodes = adj.get(node);
+        for (int i = 0; i < nodes.size(); i++) {
+            list.add(nodes.get(i));
+            if (dp.containsKey(nodes.get(i))) {
+                list.addAll(dp.get(nodes.get(i)));
+            } else {
+                Set<Integer> l = getAncestors(adj, nodes.get(i), dp);
+                if (!l.isEmpty()) {
+                    dp.put(nodes.get(i), l);
+                    list.addAll(l);
+                }
+            }
+        }
+        return list;
     }
 
 }
