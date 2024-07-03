0498bcb3cda358ed6be27104ee70a9093aacda9f
abhilash das <abhilash.das@cred.club>
2024-07-03 08:37:23 +0530

    solve: "Intersection of Two Arrays II"

--- app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
+++ app/src/main/java/app/zollet/leetcode/dsalgo/LeetCodeSolution.java
@@ -1,6 +1,12 @@
 package app.zollet.leetcode.dsalgo;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LeetCodeSolution {
 
 
@@ -8,21 +14,31 @@ public class LeetCodeSolution {
 
     }
 
+    public int[] intersect(int[] nums1, int[] nums2) {
 
-    public boolean threeConsecutiveOdds(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 != 0) count++;
-            else count = 0;
-
-            if (count == 3) return true;
-
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
         }
 
-        return false;
+        List<Integer> ans = new ArrayList<>();
 
+        for (int i = 0; i < nums2.length; i++) {
+            int count = map.getOrDefault(nums2[i], 0);
+            if (count > 0) {
+                ans.add(nums2[i]);
+                map.put(nums2[i], count - 1);
+            }
+        }
+
+        // Convert the list to an array and return
+        int[] intersectionArray = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            intersectionArray[i] = ans.get(i);
+        }
+
+        return intersectionArray;
     }
 
 }
